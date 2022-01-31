fa29fee353ca6daf6060cfb32a3ddb014e3132ef
Shawn <shawn.r.hatch@gmail.com>
2022-01-30 20:15:53 -0500

    Refactoring plugin

--- gcm3/src/main/java/nucleus/Plugin.java
+++ gcm3/src/main/java/nucleus/Plugin.java
@@ -1,13 +1,8 @@
 package nucleus;
 
-import java.util.Set;
-
-
 public interface Plugin {
 
 	public PluginId getPluginId();
-	
-	public Set<PluginId> getPluginDependencies();
 	
 	public void init(PluginContext context);
 	
