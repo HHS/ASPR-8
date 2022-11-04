179b03ac9f5793c4fdf9f2269be6fcc2463ecc94
Zachary Bischoff <bischoffz@leidos.com>
2022-11-04 04:18:03 -0400

    unit test for getPastLabel

--- gcm4/src/test/java/plugins/groups/support/AT_GroupLabeler.java
+++ gcm4/src/test/java/plugins/groups/support/AT_GroupLabeler.java
@@ -6,12 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
+import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
+import nucleus.Event;
 import nucleus.SimulationContext;
 import plugins.groups.datamanagers.GroupsDataManager;
 import plugins.groups.events.GroupMembershipAdditionEvent;
@@ -22,6 +25,7 @@ import plugins.partitions.support.LabelerSensitivity;
 import plugins.people.datamanagers.PeopleDataManager;
 import plugins.people.support.PersonError;
 import plugins.people.support.PersonId;
+import plugins.stochastics.StochasticsDataManager;
 import tools.annotations.UnitTest;
 import tools.annotations.UnitTestConstructor;
 import tools.annotations.UnitTestMethod;
@@ -131,4 +135,88 @@ public final class AT_GroupLabeler {
 		assertEquals(GroupTypeId.class, new GroupLabeler(f).getDimension());
 	}
 
+	@Test
+	@UnitTestMethod(name = "getPastLabel", args = { SimulationContext.class, Event.class })
+	public void testGetPastLabel() {
+
+		GroupsActionSupport.testConsumer(30, 3, 5, 5880749882920317232L, (c) -> {
+			PeopleDataManager peopleDataManager = c.getDataManager(PeopleDataManager.class);
+			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
+			StochasticsDataManager stochasticsDataManager = c.getDataManager(StochasticsDataManager.class);
+			RandomGenerator randomGenerator = stochasticsDataManager.getRandomGenerator();
+
+			int delta;
+			Function<GroupTypeCountMap, Object> func = (g) -> {
+				int result = 0;
+				for (GroupTypeId groupTypeId : g.getGroupTypeIds()) {
+
+					TestGroupTypeId testGroupTypeId = (TestGroupTypeId) groupTypeId;
+					result += (testGroupTypeId.ordinal() + 1) * g.getGroupCount(groupTypeId);
+				}
+				return result;
+			};
+
+			GroupLabeler groupLabeler = new GroupLabeler(func);
+
+			// Addition Events
+			GroupMembershipAdditionEvent groupMembershipAdditionEvent;
+			for (PersonId personId : peopleDataManager.getPeople()) {
+				List<GroupId> groupIdsForPersonId = groupsDataManager.getGroupsForPerson(personId);
+				int numGroupsForPerson = groupIdsForPersonId.size();
+				GroupId groupId = groupIdsForPersonId.get(randomGenerator.nextInt(numGroupsForPerson));
+				GroupTypeId expectedGroupTypeId = groupsDataManager.getGroupType(groupId);
+				
+				groupMembershipAdditionEvent = new GroupMembershipAdditionEvent(personId, groupId);
+				delta = -1;
+				GroupTypeCountMap.Builder builder = GroupTypeCountMap.builder();
+				for(GroupTypeId groupTypeId : groupsDataManager.getGroupTypeIds()){
+					int count = groupsDataManager.getGroupCountForGroupTypeAndPerson(groupTypeId, personId);
+					if(groupTypeId.equals(expectedGroupTypeId)) {
+						count += delta;
+					}
+					builder.setCount(groupTypeId, count);
+				}
+				GroupTypeCountMap groupTypeCountMap = builder.build();
+				Object expectedLabel = func.apply(groupTypeCountMap);	
+				Object actualLabel = groupLabeler.getPastLabel(c, groupMembershipAdditionEvent);
+				assertEquals(expectedLabel, actualLabel);
+			}
+			
+			// Removal Events
+			GroupMembershipRemovalEvent groupMembershipRemovalEvent;
+			for (PersonId personId : peopleDataManager.getPeople()) {
+				List<GroupId> groupIdsForPersonId = groupsDataManager.getGroupsForPerson(personId);
+				int numGroupsForPerson = groupIdsForPersonId.size();
+				GroupId groupId = groupIdsForPersonId.get(randomGenerator.nextInt(numGroupsForPerson));
+				GroupTypeId expectedGroupTypeId = groupsDataManager.getGroupType(groupId);
+				groupsDataManager.removePersonFromGroup(personId, groupId);
+
+				groupMembershipRemovalEvent = new GroupMembershipRemovalEvent(personId, groupId);
+				delta = 1;
+				GroupTypeCountMap.Builder builder = GroupTypeCountMap.builder();
+				for(GroupTypeId groupTypeId : groupsDataManager.getGroupTypeIds()){
+					int count = groupsDataManager.getGroupCountForGroupTypeAndPerson(groupTypeId, personId);
+					if(groupTypeId.equals(expectedGroupTypeId)) {
+						count += delta;
+					}
+					builder.setCount(groupTypeId, count);
+				}
+				GroupTypeCountMap groupTypeCountMap = builder.build();
+				Object expectedLabel = func.apply(groupTypeCountMap);	
+				Object actualLabel = groupLabeler.getPastLabel(c, groupMembershipRemovalEvent);
+				assertEquals(expectedLabel, actualLabel);
+			}			
+			//precondition tests
+			
+			GroupId groupId = groupsDataManager.getGroupIds().get(0);
+			//if the person id is null
+			ContractException contractException = assertThrows(ContractException.class,()-> groupLabeler.getPastLabel(c, new GroupMembershipAdditionEvent(null, groupId)));
+			assertEquals(PersonError.NULL_PERSON_ID, contractException.getErrorType());
+			
+			//if the person id is unknown
+			contractException = assertThrows(ContractException.class,()-> groupLabeler.getPastLabel(c, new GroupMembershipAdditionEvent(new PersonId(100000), groupId)));
+			assertEquals(PersonError.UNKNOWN_PERSON_ID, contractException.getErrorType());
+
+		});
+	}
 }
