9ed8d822cf25844f502a9aa04d4f004d1de41590
Zachary Bischoff <bischoffz@leidos.com>
2022-11-04 03:27:48 -0400

    unit test for validate method

--- gcm4/src/test/java/plugins/groups/support/AT_GroupMemberFilter.java
+++ gcm4/src/test/java/plugins/groups/support/AT_GroupMemberFilter.java
@@ -1,5 +1,6 @@
 package plugins.groups.support;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,8 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-
-
 
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
@@ -35,7 +34,7 @@ import util.errors.ContractException;
 public class AT_GroupMemberFilter {
 
 	@Test
-	@UnitTestConstructor(args = {GroupId.class })
+	@UnitTestConstructor(args = { GroupId.class })
 	public void testConstructor() {
 
 		GroupsActionSupport.testConsumer(100, 3, 10, 8499169041100865476L, (c) -> {
@@ -46,23 +45,23 @@ public class AT_GroupMemberFilter {
 				final Filter filter = new GroupMemberFilter(groupId);
 				assertNotNull(filter);
 			}
-			
 
 			// precondition tests
-			ContractException contractException = assertThrows(ContractException.class, () -> new GroupMemberFilter(null).validate(c));
+			ContractException contractException = assertThrows(ContractException.class,
+					() -> new GroupMemberFilter(null).validate(c));
 			assertEquals(GroupError.NULL_GROUP_ID, contractException.getErrorType());
-		});		
+		});
 	}
 
 	@Test
 	@UnitTestMethod(name = "getFilterSensitivities", args = {})
 	public void testGetFilterSensitivities() {
-		
+
 		GroupsActionSupport.testConsumer(100, 3, 10, 7283631979607042406L, (c) -> {
 			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
-			
+
 			GroupId groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_1);
-			 
+
 			Filter filter = new GroupMemberFilter(groupId);
 
 			Set<Class<?>> expected = new LinkedHashSet<>();
@@ -79,30 +78,30 @@ public class AT_GroupMemberFilter {
 				actual.add(eventClass);
 			}
 			assertEquals(expected, actual);
-			
+
 		});
-		
+
 	}
 
 	@Test
 	@UnitTestMethod(name = "evaluate", args = { SimulationContext.class, PersonId.class })
 	public void testEvaluate() {
-		
+
 		GroupsActionSupport.testConsumer(100, 3, 10, 6248106595116941770L, (c) -> {
 			RandomGenerator randomGenerator = c.getDataManager(StochasticsDataManager.class).getRandomGenerator();
 			PeopleDataManager peopleDataManager = c.getDataManager(PeopleDataManager.class);
 			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
 			GroupId groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_3);
 			Filter filter = new GroupMemberFilter(groupId);
-			
+
 			for (PersonId personId : peopleDataManager.getPeople()) {
 				if (randomGenerator.nextBoolean()) {
-					groupsDataManager.addPersonToGroup(personId,groupId);					
+					groupsDataManager.addPersonToGroup(personId, groupId);
 				}
 			}
 
 			for (PersonId personId : peopleDataManager.getPeople()) {
-				boolean expected = groupsDataManager.isPersonInGroup(personId,groupId);
+				boolean expected = groupsDataManager.isPersonInGroup(personId, groupId);
 				boolean actual = filter.evaluate(c, personId);
 				assertEquals(expected, actual);
 			}
@@ -112,8 +111,29 @@ public class AT_GroupMemberFilter {
 			assertEquals(PersonError.NULL_PERSON_ID, contractException.getErrorType());
 
 			/* precondition: if the person id is unknown */
-			contractException = assertThrows(ContractException.class, () -> filter.evaluate(c, new PersonId(123412342)));
+			contractException = assertThrows(ContractException.class,
+					() -> filter.evaluate(c, new PersonId(123412342)));
 			assertEquals(PersonError.UNKNOWN_PERSON_ID, contractException.getErrorType());
+		});
+	}
+
+	@Test
+	@UnitTestMethod(name = "validate", args = { SimulationContext.class })
+	public void testValidate() {
+
+		GroupsActionSupport.testConsumer(100, 3, 10, 6248106595116941770L, (c) -> {
+			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
+			GroupId groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_3);
+			Filter filter = new GroupMemberFilter(groupId);
+
+			/* precondition: if the groupId is null */
+			ContractException contractException = assertThrows(ContractException.class,
+					() -> new GroupMemberFilter(null).validate(c));
+			assertEquals(GroupError.NULL_GROUP_ID, contractException.getErrorType());
+
+			// show that a properly defined filter validates and does not throw
+			assertDoesNotThrow(() -> filter.validate(c));
+
 		});
 	}
 }
