d3ea9369e5470c22125f5ecbb905ab4ba96cfbfe
Baldwin-Akin Varner <akinvarner123@gmail.com>
2022-11-07 11:44:51 -0500

    Fixed GlobalPropertiesPluginData issue

--- gcm4/src/main/java/plugins/globalproperties/GlobalPropertiesPluginData.java
+++ gcm4/src/main/java/plugins/globalproperties/GlobalPropertiesPluginData.java
@@ -188,7 +188,7 @@ public final class GlobalPropertiesPluginData implements PluginData {
 
 		private Data(Data data) {
 			globalPropertyDefinitions.putAll(data.globalPropertyDefinitions);
-			globalPropertyValues.putAll(globalPropertyValues);
+			globalPropertyValues.putAll(this.globalPropertyValues);
 		}
 	}
 
