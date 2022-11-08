a54fe8f5300bd5a0f47cd93ef7e8c7e30e00768c
Baldwin-Akin Varner <akinvarner123@gmail.com>
2022-11-08 09:23:37 -0500

    Added missing tests for TestGlobalPropertyId

--- gcm4/src/test/java/plugins/globalproperties/testsupport/AT_TestGlobalPropertyId.java
+++ gcm4/src/test/java/plugins/globalproperties/testsupport/AT_TestGlobalPropertyId.java
@@ -1,21 +1,57 @@
 package plugins.globalproperties.testsupport;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
 import plugins.globalproperties.support.GlobalPropertyId;
+import plugins.groups.testsupport.TestGroupPropertyId;
+import plugins.util.properties.PropertyDefinition;
 import tools.annotations.UnitTest;
 import tools.annotations.UnitTestMethod;
+import util.random.RandomGeneratorProvider;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @UnitTest(target = TestGlobalPropertyId.class)
 public class AT_TestGlobalPropertyId {
 
+	@Test
+	@UnitTestMethod(name = "getPropertyDefinition", args = {})
+	public void testGetPropertyDefinition() {
+		for (TestGlobalPropertyId testGlobalPropertyId : TestGlobalPropertyId.values()) {
+			assertNotNull(testGlobalPropertyId.getPropertyDefinition());
+		}
+	}
+
+	@Test
+	@UnitTestMethod(name = "getRandomPropertyValue", args = { RandomGenerator.class })
+	public void testGetRandomPropertyValue() {
+		RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(6173923848365818813L);
+
+		/*
+		 * Show that randomly generated values are compatible with the
+		 * associated property definition. Show that the values are reasonably
+		 * unique
+		 */
+		for (TestGlobalPropertyId testGlobalPropertyId : TestGlobalPropertyId.values()) {
+			PropertyDefinition propertyDefinition = testGlobalPropertyId.getPropertyDefinition();
+			Set<Object> values = new LinkedHashSet<>();
+			for (int i = 0; i < 100; i++) {
+				Object propertyValue = testGlobalPropertyId.getRandomPropertyValue(randomGenerator);
+				values.add(propertyValue);
+				assertTrue(propertyDefinition.getType().isAssignableFrom(propertyValue.getClass()));
+			}
+			//show that the values are reasonable unique
+			if (propertyDefinition.getType() != Boolean.class) {
+				assertTrue(values.size() > 10);
+			} else {
+				assertEquals(2, values.size());
+			}
+		}
+	}
 	
 	@Test
 	@UnitTestMethod(name = "getUnknownGlobalPropertyId", args = {})
