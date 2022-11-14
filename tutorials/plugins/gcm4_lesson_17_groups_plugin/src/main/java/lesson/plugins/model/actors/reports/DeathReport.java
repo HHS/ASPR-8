1927ea6096c9c3b5dd81187356efcfa30459ef7b
Shawn <shawn.r.hatch@gmail.com>
2022-11-14 12:46:04 -0500

    Updated modeling guide and tutorial code for groups plugin

---
+++ tutorials/plugins/gcm4_lesson_17_groups_plugin/src/main/java/lesson/plugins/model/actors/reports/DeathReport.java
@@ -0,0 +1,96 @@
+package lesson.plugins.model.actors.reports;
+
+import java.util.List;
+
+import lesson.plugins.model.PersonProperty;
+import nucleus.ActorContext;
+import plugins.people.support.PersonId;
+import plugins.personproperties.datamanagers.PersonPropertiesDataManager;
+import plugins.regions.datamanagers.RegionsDataManager;
+import plugins.regions.support.RegionId;
+import plugins.reports.support.ReportHeader;
+import plugins.reports.support.ReportId;
+import plugins.reports.support.ReportItem;
+
+/**
+ * A report that groups people at the end of the simulation by their shared
+ * person property values.
+ * 
+ * @author Shawn Hatch
+ *
+ */
+public final class DeathReport {
+	private final ReportId reportId;
+
+	public DeathReport(ReportId reportId) {
+		this.reportId = reportId;
+	}
+
+	public void init(ActorContext actorContext) {
+		actorContext.subscribeToSimulationClose(this::report);
+	}
+
+	private void report(ActorContext actorContext) {
+		RegionsDataManager regionsDataManager = actorContext.getDataManager(RegionsDataManager.class);
+		PersonPropertiesDataManager personPropertiesDataManager = actorContext.getDataManager(PersonPropertiesDataManager.class);
+		
+		ReportHeader reportHeader = ReportHeader.builder()//
+												.add("region")//
+												.add("pop_size")//
+												.add("deaths")//
+												.add("deaths_in_home")//
+												.add("deaths_in_hospital")//
+												.add("per_capita_deaths")//
+												.add("per_capita_deaths_in_home")//
+												.add("per_capita_deaths_in_hospital")//												
+												.build();
+		ReportItem.Builder reportItemBuilder = ReportItem.builder();
+
+		for (RegionId regionId : regionsDataManager.getRegionIds()) {
+			List<PersonId> peopleInRegion = regionsDataManager.getPeopleInRegion(regionId);
+			int popSize = peopleInRegion.size();
+			int homeDeathCount = 0;
+			int hospitalDeathCount = 0;
+			
+			for (PersonId personId : peopleInRegion) {
+				Boolean deadInHospital = personPropertiesDataManager.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOSPITAL);
+				if (deadInHospital) {
+					hospitalDeathCount++;
+				}
+				Boolean deadInHome = personPropertiesDataManager.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOME);
+				if (deadInHome) {
+					homeDeathCount++;
+				}
+			}
+			int deathCount = homeDeathCount + hospitalDeathCount;
+			double perCapitaDeaths = 0;
+			double perCapitaHomeDeaths = 0;
+			double perCapitaHospitalDeaths = 0;
+			if (peopleInRegion.size() > 0) {
+				perCapitaDeaths = ((double)deathCount)/popSize;
+				perCapitaHomeDeaths = ((double)homeDeathCount)/popSize;
+				perCapitaHospitalDeaths = ((double)hospitalDeathCount)/popSize;
+
+			}
+			reportItemBuilder.setReportHeader(reportHeader);
+			reportItemBuilder.setReportId(reportId);
+			reportItemBuilder.addValue(regionId);
+			reportItemBuilder.addValue(peopleInRegion.size());
+			reportItemBuilder.addValue(deathCount);
+			reportItemBuilder.addValue(homeDeathCount);
+			reportItemBuilder.addValue(hospitalDeathCount);
+			reportItemBuilder.addValue(perCapitaDeaths);
+			reportItemBuilder.addValue(perCapitaHomeDeaths);
+			reportItemBuilder.addValue(perCapitaHospitalDeaths);
+
+
+			ReportItem reportItem = reportItemBuilder.build();
+			/*
+			 * Release the report item from the simulation
+			 */
+			actorContext.releaseOutput(reportItem);
+
+		}
+
+	}
+}
--- tutorials/plugins/gcm4_lesson_18_resources_plugin/src/main/java/lesson/plugins/model/actors/QuestionnaireDistributor.java
+++ tutorials/plugins/gcm4_lesson_18_resources_plugin/src/main/java/lesson/plugins/model/actors/QuestionnaireDistributor.java
@@ -13,30 +13,38 @@ public class QuestionnaireDistributor {
 	private PersonPropertiesDataManager personPropertiesDataManager;
 
 	public void init(ActorContext actorContext) {
-		ResourcesDataManager resourcesDataManager = actorContext.getDataManager(ResourcesDataManager.class);
-		personPropertiesDataManager = actorContext.getDataManager(PersonPropertiesDataManager.class);
+		ResourcesDataManager resourcesDataManager = actorContext
+				.getDataManager(ResourcesDataManager.class);
+		personPropertiesDataManager = actorContext
+				.getDataManager(PersonPropertiesDataManager.class);
 		
 
-		EventFilter<PersonResourceUpdateEvent> eventFilter = resourcesDataManager.getEventFilterForPersonResourceUpdateEvent(Resource.ANTI_VIRAL_MED);
+		EventFilter<PersonResourceUpdateEvent> eventFilter = 
+				resourcesDataManager
+				.getEventFilterForPersonResourceUpdateEvent(Resource.ANTI_VIRAL_MED);
 		actorContext.subscribe(eventFilter, this::handleAntiViralDistribution);
 
-		eventFilter = resourcesDataManager.getEventFilterForPersonResourceUpdateEvent(Resource.HOSPITAL_BED);
+		eventFilter = resourcesDataManager
+				.getEventFilterForPersonResourceUpdateEvent(Resource.HOSPITAL_BED);
 		actorContext.subscribe(eventFilter, this::handleAntiHospitalBedDistribution);
 
 	}
 
-	private void handleAntiViralDistribution(ActorContext actorContext, PersonResourceUpdateEvent personResourceUpdateEvent) {
+	private void handleAntiViralDistribution(ActorContext actorContext,
+			PersonResourceUpdateEvent personResourceUpdateEvent) {
 		PersonId personId = personResourceUpdateEvent.getPersonId();
 		boolean hasAntiviral = personResourceUpdateEvent.getCurrentResourceLevel() > 0;
 		if (!hasAntiviral) {
-			boolean hospitalized = personPropertiesDataManager.getPersonPropertyValue(personId, PersonProperty.HOSPITALIZED);
+			boolean hospitalized = personPropertiesDataManager
+					.getPersonPropertyValue(personId, PersonProperty.HOSPITALIZED);
 			if (!hospitalized) {
 				distributeQuestionaire(personId);
 			}
 		}
 	}
 
-	private void handleAntiHospitalBedDistribution(ActorContext actorContext, PersonResourceUpdateEvent personResourceUpdateEvent) {
+	private void handleAntiHospitalBedDistribution(ActorContext actorContext
+			, PersonResourceUpdateEvent personResourceUpdateEvent) {
 		PersonId personId = personResourceUpdateEvent.getPersonId();
 		boolean hasBed = personResourceUpdateEvent.getCurrentResourceLevel() > 0;
 		boolean dead = personIsDead(personId);
@@ -46,12 +54,15 @@ public class QuestionnaireDistributor {
 	}
 
 	private boolean personIsDead(PersonId personId) {
-		boolean deadInHome = personPropertiesDataManager.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOME);
-		boolean deadInHospital = personPropertiesDataManager.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOSPITAL);
+		boolean deadInHome = personPropertiesDataManager
+				.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOME);
+		boolean deadInHospital = personPropertiesDataManager
+				.getPersonPropertyValue(personId, PersonProperty.DEAD_IN_HOSPITAL);
 		return deadInHome || deadInHospital;
 	}
 
 	private void distributeQuestionaire(PersonId personId) {
-		personPropertiesDataManager.setPersonPropertyValue(personId, PersonProperty.RECEIVED_QUESTIONNAIRE, true);
+		personPropertiesDataManager
+			.setPersonPropertyValue(personId, PersonProperty.RECEIVED_QUESTIONNAIRE, true);
 	}
 }
