56faceb78881255136552ad61743c07e185455cf
Baldwin-Akin Varner <akinvarner123@gmail.com>
2022-11-17 08:33:22 -0500

    Updated GlobalPropertyDefinitionEvent test

--- gcm4/src/test/java/plugins/globalproperties/events/AT_GlobalPropertyDefinitionEvent.java
+++ gcm4/src/test/java/plugins/globalproperties/events/AT_GlobalPropertyDefinitionEvent.java
@@ -5,6 +5,7 @@ import plugins.globalproperties.support.SimpleGlobalPropertyId;
 import plugins.util.properties.PropertyError;
 import tools.annotations.UnitTest;
 import tools.annotations.UnitTestConstructor;
+import tools.annotations.UnitTestMethod;
 import util.errors.ContractException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,11 +24,37 @@ public class AT_GlobalPropertyDefinitionEvent {
 
     @Test
     @UnitTestConstructor(args = {})
-    void initialPropertyValue() {
+    public void initialPropertyValue() {
 
         SimpleGlobalPropertyId goodId = new SimpleGlobalPropertyId(5);
 
         ContractException contractException = assertThrows(ContractException.class, () -> new GlobalPropertyDefinitionEvent(goodId, null));
         assertEquals(PropertyError.NULL_PROPERTY_VALUE, contractException.getErrorType());
     }
+
+    @Test
+    @UnitTestMethod(name = "equals", args = {Object.class})
+    public void testEquals() {
+        // nothing to test
+    }
+
+    @Test
+    @UnitTestMethod(name = "toString", args = {})
+    public void testToString() {
+        // nothing to test
+    }
+
+    @Test
+    @UnitTestMethod(name = "hashCode", args = {})
+    public void testHashCode() {
+        // nothing to test
+    }
+
+    @Test
+    @UnitTestMethod(name = "initialPropertyValue", args = {})
+    public void testInitialPropertyValue() {
+        // nothing to test
+    }
+
+
 }
