b504467d75cb139bba04b850adae783c8d5d80d3
Shawn <shawn.r.hatch@gmail.com>
2022-11-21 10:43:19 -0500

    Updated modeling guide and tutorial code for groups plugin

--- tutorials/plugins/gcm4_lesson_17_groups_plugin/src/main/java/lesson/plugins/model/support/GroupProperty.java
+++ tutorials/plugins/gcm4_lesson_17_groups_plugin/src/main/java/lesson/plugins/model/support/GroupProperty.java
@@ -3,5 +3,7 @@ package lesson.plugins.model.support;
 import plugins.groups.support.GroupPropertyId;
 
 public enum GroupProperty implements GroupPropertyId {
-	TELEWORK
+	TELEWORK,
+	SCHOOL_STATUS,
+	;
 }
