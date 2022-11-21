96e26a71d056ba17b641cd88faf332bd21860d5c
bischoffz <bischoffz@leidos.com>
2022-11-21 13:33:52 -0500

    remove unsued import

--- gcm4/src/test/java/plugins/regions/testsupport/AT_TestRegionPropertyId.java
+++ gcm4/src/test/java/plugins/regions/testsupport/AT_TestRegionPropertyId.java
@@ -1,7 +1,6 @@
 package plugins.regions.testsupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
