8a5a78f231ddd815ec32aa1a782172e03c15d1b6
Baldwin-Akin Varner <akinvarner123@gmail.com>
2022-12-05 07:45:50 -0500

    Updated TestResourceId Test

--- gcm4/src/test/java/plugins/resources/testsupport/AT_TestResourceId.java
+++ gcm4/src/test/java/plugins/resources/testsupport/AT_TestResourceId.java
@@ -2,16 +2,15 @@ package plugins.resources.testsupport;
 
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
+import plugins.groups.testsupport.TestGroupTypeId;
+import plugins.resources.support.ResourceId;
 import tools.annotations.UnitTag;
 import tools.annotations.UnitTest;
 import tools.annotations.UnitTestMethod;
 import util.random.RandomGeneratorProvider;
 import util.wrappers.MutableInteger;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,7 +30,6 @@ public class AT_TestResourceId {
     public void testGetRandomResourceId() {
         RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(5357990509395444631L);
         Map<TestResourceId, MutableInteger> idCounter = new LinkedHashMap<>();
-        Set<TestResourceId> setOfTestResourceIds = new LinkedHashSet<>();
 
         for (TestResourceId testResourceId : TestResourceId.values()) {
             idCounter.put(testResourceId, new MutableInteger());
@@ -39,20 +37,28 @@ public class AT_TestResourceId {
 
         for (int i = 0; i < 50; i++) {
             TestResourceId testResourceId = TestResourceId.getRandomResourceId(randomGenerator);
-            setOfTestResourceIds.add(testResourceId);
             idCounter.get(testResourceId).increment();
         }
 
         for (TestResourceId testResourceId : idCounter.keySet()) {
             assertTrue(idCounter.get(testResourceId).getValue() >= 5 && idCounter.get(testResourceId).getValue() <= 30);
         }
-        assertEquals(setOfTestResourceIds.size(), 5);
     }
 
     @Test
     @UnitTestMethod(name = "getUnknownResourceId", args = {})
     public void testGetUnknownResourceId() {
-        assertNotNull(TestResourceId.getUnknownResourceId());
+        Set<TestResourceId> oldIds = EnumSet.allOf(TestResourceId.class);
+        Set<ResourceId> unknownIds = new LinkedHashSet<>();
+
+        // show that each unknown id is not null and unique
+        for(int i = 0; i < 50; i++) {
+            ResourceId unknownResourceId = TestResourceId.getUnknownResourceId();
+            assertNotNull(unknownResourceId);
+            boolean unique = unknownIds.add(unknownResourceId);
+            assertTrue(unique);
+            assertFalse(oldIds.contains(unknownResourceId));
+        }
     }
 
     @Test
@@ -65,8 +71,12 @@ public class AT_TestResourceId {
     @Test
     @UnitTestMethod(name = "next", args = {}, tags = UnitTag.INCOMPLETE)
     public void testNext() {
-        TestResourceId testResourceId = TestResourceId.RESOURCE_1;
-        assertNotNull(testResourceId.next());
+        for (TestResourceId testResourceId : TestResourceId.values()) {
+            int index = (testResourceId.ordinal() + 1) % TestResourceId.values().length;
+            TestResourceId expectedNext = TestResourceId.values()[index];
+            assertNotNull(testResourceId.next());
+            assertEquals(expectedNext, testResourceId.next());
+        }
     }
 
 }
