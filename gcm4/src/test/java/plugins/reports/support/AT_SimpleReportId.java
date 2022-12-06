f71e8e9f71929cb3e596b761290902faff1d7f0b
Baldwin-Akin Varner <akinvarner123@gmail.com>
2022-12-06 09:10:56 -0500

    Updated SimpleReportId Test
    Updated equals test and added hashcode test

--- gcm4/src/test/java/plugins/reports/support/AT_SimpleReportId.java
+++ gcm4/src/test/java/plugins/reports/support/AT_SimpleReportId.java
@@ -8,6 +8,9 @@ import tools.annotations.UnitTestConstructor;
 import tools.annotations.UnitTestMethod;
 import util.errors.ContractException;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @UnitTest(target = SimpleReportId.class)
@@ -16,7 +19,7 @@ public class AT_SimpleReportId {
     @Test
     @UnitTestConstructor(args = {Object.class})
     public void testConstructor() {
-        Object value = 1;
+        assertNotNull(new SimpleReportId(5));
 
         // show that a null report id is not thrown
         ContractException contractException = assertThrows(ContractException.class, () -> new SimpleReportId(null));
@@ -36,18 +39,89 @@ public class AT_SimpleReportId {
     }
 
     @Test
+    @UnitTestMethod(name = "hashCode", args = {})
+    public void testHashCode() {
+        // equal objects have equal hash codes
+        for (int i = 0; i < 30; i++) {
+            SimpleReportId s1 = new SimpleReportId(i);
+            SimpleReportId s2 = new SimpleReportId(i);
+            assertEquals(s1.hashCode(), s2.hashCode());
+        }
+
+        Set<Integer> hashCodes = new LinkedHashSet<>();
+        for (int i = 0; i < 30; i++) {
+            boolean unique = hashCodes.add(new SimpleReportId(i).hashCode());
+            assertTrue(unique);
+        }
+    }
+
+    @Test
     @UnitTestMethod(name = "equals", args = {Object.class}, tags = UnitTag.INCOMPLETE)
     public void testEquals() {
-        Object value = 730;
-        SimpleReportId simpleReportId1 = new SimpleReportId(value);
-        SimpleReportId simpleReportId2 = new SimpleReportId(value);
+        Object value = 2;
+        SimpleReportId id_1 = new SimpleReportId(2);
+        SimpleReportId id_2 = new SimpleReportId(5);
+        SimpleReportId id_3 = new SimpleReportId(2);
+        SimpleReportId id_4 = new SimpleReportId("A");
+        SimpleReportId id_5 = new SimpleReportId("A");
+        SimpleReportId id_6 = new SimpleReportId("B");
+        SimpleReportId id_7 = new SimpleReportId("A");
         SimpleGlobalPropertyId simpleGlobalPropertyId = new SimpleGlobalPropertyId(value);
 
-        // show that objects created with the same value are equal
-        assertTrue(simpleReportId1.equals(simpleReportId2));
-
         // should return false if the object is not a SimpleReportId
-        assertFalse(simpleReportId1.equals(simpleGlobalPropertyId));
+        assertNotEquals(id_1, simpleGlobalPropertyId);
+
+        assertEquals(id_1, id_1); 		// testing reflexive property
+        assertNotEquals(id_1, id_2);
+        assertEquals(id_1, id_3);		// part of reflective property test
+        assertNotEquals(id_1, id_4);
+        assertNotEquals(id_1, id_5);
+        assertNotEquals(id_1, id_6);
+
+        assertNotEquals(id_2, id_1);
+        assertEquals(id_2, id_2);
+        assertNotEquals(id_2, id_3);
+        assertNotEquals(id_2, id_4);
+        assertNotEquals(id_2, id_5);
+        assertNotEquals(id_2, id_6);
+
+        assertEquals(id_3, id_1);		// part of reflective property test
+        assertNotEquals(id_3, id_2);
+        assertEquals(id_3, id_3);
+        assertNotEquals(id_3, id_4);
+        assertNotEquals(id_3, id_5);
+        assertNotEquals(id_3, id_6);
+
+        assertNotEquals(id_4, id_1);
+        assertNotEquals(id_4, id_2);
+        assertNotEquals(id_4, id_3);
+        assertEquals(id_4, id_4);
+        assertEquals(id_4, id_5);		// part of transitive property test
+        assertNotEquals(id_4, id_6);
+        assertEquals(id_4, id_7);		// part of transitive property test
+
+        assertNotEquals(id_5, id_1);
+        assertNotEquals(id_5, id_2);
+        assertNotEquals(id_5, id_3);
+        assertEquals(id_5, id_4);
+        assertEquals(id_5, id_5);
+        assertNotEquals(id_5, id_6);
+        assertEquals(id_5, id_7);		// part of transitive property test
+
+        assertNotEquals(id_6, id_1);
+        assertNotEquals(id_6, id_2);
+        assertNotEquals(id_6, id_3);
+        assertNotEquals(id_6, id_4);
+        assertNotEquals(id_6, id_5);
+        assertEquals(id_6, id_6);
+
+        // null tests
+        assertNotEquals(id_1, null);
+        assertNotEquals(id_2, null);
+        assertNotEquals(id_3, null);
+        assertNotEquals(id_4, null);
+        assertNotEquals(id_5, null);
+        assertNotEquals(id_6, null);
     }
 
 }
