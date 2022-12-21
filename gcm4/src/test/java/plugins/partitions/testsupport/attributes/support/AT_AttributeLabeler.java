6d264cc6c1a0a8eb36db1ad7dd4c80cf20086dca
bischoffz <bischoffz@leidos.com>
2022-12-21 15:40:15 -0500

    unit tests for attribute labeler

--- gcm4/src/test/java/plugins/partitions/testsupport/attributes/support/AT_AttributeLabeler.java
+++ gcm4/src/test/java/plugins/partitions/testsupport/attributes/support/AT_AttributeLabeler.java
@@ -9,8 +9,10 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
+import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
+import nucleus.Event;
 import nucleus.Plugin;
 import nucleus.SimulationContext;
 import nucleus.testsupport.testplugin.TestActorPlan;
@@ -27,6 +29,7 @@ import tools.annotations.UnitTest;
 import tools.annotations.UnitTestConstructor;
 import tools.annotations.UnitTestMethod;
 import util.errors.ContractException;
+import util.random.RandomGeneratorProvider;
 
 @UnitTest(target = AttributeLabeler.class)
 public final class AT_AttributeLabeler {
@@ -38,7 +41,7 @@ public final class AT_AttributeLabeler {
 	}
 
 	@Test
-	@UnitTestMethod(name = "getLabelerSensitivities", args = { })
+	@UnitTestMethod(name = "getLabelerSensitivities", args = {})
 	public void testGetLabelerSensitivities() {
 		/*
 		 * Get the labeler sensitivities and show that they are consistent with
@@ -60,7 +63,8 @@ public final class AT_AttributeLabeler {
 		// show that the sensitivity will return the person id from a
 		// AttributeUpdateEvent
 		PersonId personId = new PersonId(56);
-		AttributeUpdateEvent attributeUpdateEvent = new AttributeUpdateEvent(personId, TestAttributeId.BOOLEAN_0, false, true);
+		AttributeUpdateEvent attributeUpdateEvent = new AttributeUpdateEvent(personId, TestAttributeId.BOOLEAN_0, false,
+				true);
 		Optional<PersonId> optional = labelerSensitivity.getPersonId(attributeUpdateEvent);
 		assertTrue(optional.isPresent());
 		assertEquals(personId, optional.get());
@@ -82,8 +86,6 @@ public final class AT_AttributeLabeler {
 		AttributeLabeler attributeLabeler = new AttributeLabeler(TestAttributeId.BOOLEAN_0, function);
 
 		TestPluginData.Builder pluginBuilder = TestPluginData.builder();
-
-		
 
 		/*
 		 * 
@@ -112,7 +114,8 @@ public final class AT_AttributeLabeler {
 		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(1, (c) -> {
 
 			// if the person does not exist
-			ContractException contractException = assertThrows(ContractException.class, () -> attributeLabeler.getLabel(c, new PersonId(100000)));
+			ContractException contractException = assertThrows(ContractException.class,
+					() -> attributeLabeler.getLabel(c, new PersonId(100000)));
 			assertEquals(PersonError.UNKNOWN_PERSON_ID, contractException.getErrorType());
 
 			// if the person id is null
@@ -127,10 +130,51 @@ public final class AT_AttributeLabeler {
 
 	}
 
+	@Test
+	@UnitTestMethod(name = "getPastLabel", args = { SimulationContext.class, Event.class })
 	public void testGetPastLabel() {
+		Function<Object, Object> function = (c) -> {
+			return c;
+		};
 
+		AttributeLabeler attributeLabeler = new AttributeLabeler(TestAttributeId.INT_0, function);
+
+		TestPluginData.Builder pluginBuilder = TestPluginData.builder();
+		RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(8705700576614764378L);
+
+		/*
+		 * 
+		 */
+		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(0, (c) -> {
+			PeopleDataManager peopleDataManager = c.getDataManager(PeopleDataManager.class);
+			AttributesDataManager attributesDataManager = c.getDataManager(AttributesDataManager.class);
+			List<PersonId> people = peopleDataManager.getPeople();
+
+			AttributeUpdateEvent event;
+			for (PersonId personId : people) {
+
+				// get the person's attribute value and apply the function
+				// directly
+				int prev = attributesDataManager.getAttributeValue(personId, TestAttributeId.INT_0);
+				int next = randomGenerator.nextInt(100);
+				attributesDataManager.setAttributeValue(personId, TestAttributeId.INT_0, next);
+				event = new AttributeUpdateEvent(personId, TestAttributeId.INT_0, prev, next);
+				Object expectedLabel = function.apply(prev);
+
+				// get the label from the person id
+				Object actualLabel = attributeLabeler.getPastLabel(c, event);
+
+				// show that the two labels are equal
+				assertEquals(expectedLabel, actualLabel);
+
+			}
+		}));
+
+		TestPluginData testPluginData = pluginBuilder.build();
+		Plugin testPlugin = TestPlugin.getTestPlugin(testPluginData);
+		PartitionsActionSupport.testConsumers(10, 4161680035971681080L, testPlugin);
 	}
-	
+
 	@Test
 	@UnitTestMethod(name = "getDimension", args = {})
 	public void testGetDimension() {
