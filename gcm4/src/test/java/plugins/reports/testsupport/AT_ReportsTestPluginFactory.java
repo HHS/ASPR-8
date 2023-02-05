fb2508933d6fb8b8726c683dd0aed260686c6a39
Shawn <shawn.r.hatch@gmail.com>
2023-02-05 03:05:31 -0500

    Completed test of ReportsTestPluginFactory

--- gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
+++ gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
@@ -1,18 +1,33 @@
 package plugins.reports.testsupport;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 
+import nucleus.Plugin;
+import nucleus.Simulation;
+import nucleus.testsupport.testplugin.TestSimulation;
 import tools.annotations.UnitTag;
 import tools.annotations.UnitTestMethod;
+import util.wrappers.MutableBoolean;
 
 public class AT_ReportsTestPluginFactory {
 
 	@Test
-	@UnitTestMethod(target = ReportsTestPluginFactory.class, name = "getPluginFromReport", args= {Consumer.class}, tags= {UnitTag.INCOMPLETE})
+	@UnitTestMethod(target = ReportsTestPluginFactory.class, name = "getPluginFromReport", args= {Consumer.class})
 	public void testGetPluginFromReport() {
-		        
+		
+		MutableBoolean executed = new MutableBoolean();
+		
+		Plugin plugin = ReportsTestPluginFactory.getPluginFromReport((c)->executed.setValue(true));
+		
+		Simulation.builder().addPlugin(plugin).build().execute();
+		
+		assertTrue(executed.getValue());
     }
 	
 }
