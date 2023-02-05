1f594f869ea73af78f03bd42f83645494ab4f7d9
Shawn <shawn.r.hatch@gmail.com>
2023-02-05 03:10:34 -0500

    Documented test of ReportsTestPluginFactory

--- gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
+++ gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
@@ -14,16 +14,31 @@ import util.wrappers.MutableBoolean;
 public class AT_ReportsTestPluginFactory {
 
 	@Test
-	@UnitTestMethod(target = ReportsTestPluginFactory.class, name = "getPluginFromReport", args= {Consumer.class})
+	@UnitTestMethod(target = ReportsTestPluginFactory.class, name = "getPluginFromReport", args = { Consumer.class })
 	public void testGetPluginFromReport() {
-		
+
+		/*
+		 * Create a boolean defaulted to false.
+		 */
 		MutableBoolean executed = new MutableBoolean();
-		
-		Plugin plugin = ReportsTestPluginFactory.getPluginFromReport((c)->executed.setValue(true));
-		
+
+		/*
+		 * Create a report and get the corresponding plugin. if the report was
+		 * added then it should get executed by a simulation and the boolean
+		 * will be set to true.
+		 */
+		Plugin plugin = ReportsTestPluginFactory.getPluginFromReport((c) -> executed.setValue(true));
+
+		/*
+		 * Execute a simulation with the plugin.
+		 */
 		Simulation.builder().addPlugin(plugin).build().execute();
-		
+
+		/*
+		 * Show that the report executed and thus must have been properly added
+		 * to the plugin.
+		 */
 		assertTrue(executed.getValue());
-    }
-	
+	}
+
 }
