fea6c0353106c0aecd12b261101ff0581d071211
Shawn <shawn.r.hatch@gmail.com>
2023-02-05 03:08:40 -0500

    Completed test of ReportsTestPluginFactory

--- gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
+++ gcm4/src/test/java/plugins/reports/testsupport/AT_ReportsTestPluginFactory.java
@@ -2,16 +2,12 @@ package plugins.reports.testsupport;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 
 import nucleus.Plugin;
 import nucleus.Simulation;
-import nucleus.testsupport.testplugin.TestSimulation;
-import tools.annotations.UnitTag;
 import tools.annotations.UnitTestMethod;
 import util.wrappers.MutableBoolean;
 
