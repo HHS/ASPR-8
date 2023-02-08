d9c1c4ede36f94e6f2b88655d0262c4e94ecad81
Shawn <shawn.r.hatch@gmail.com>
2023-02-08 13:26:27 -0500

    added equals contract specification to plugin id

--- gcm4/src/main/java/nucleus/PluginId.java
+++ gcm4/src/main/java/nucleus/PluginId.java
@@ -10,5 +10,16 @@ import net.jcip.annotations.ThreadSafe;
  */
 @ThreadSafe
 public interface PluginId {
+	/**
+	 * Implementationn consistent with equals()
+	 */
+	@Override
+	public int hashCode();
 
+	/**
+	 * Two plugin ids are equal if and only if they represent the same plugin.
+	 * Plugin ids are generally implemented as static instances.
+	 */
+	@Override
+	public boolean equals(Object obj);
 }
