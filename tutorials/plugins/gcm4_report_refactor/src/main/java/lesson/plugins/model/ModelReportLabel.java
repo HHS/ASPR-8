ee1888ed4d6baa90f7e41ea2d868716646a5953d
Baldwin-Akin Varner <akinvarner123@gmail.com>
2023-02-10 12:01:45 -0500

    added tutorial 18

---
+++ tutorials/plugins/gcm4_report_refactor/src/main/java/lesson/plugins/model/ModelReportLabel.java
@@ -0,0 +1,11 @@
+package lesson.plugins.model;
+
+import plugins.reports.support.ReportLabel;
+
+public enum ModelReportLabel implements ReportLabel{	
+	PERSON_RESOURCE_REPORT,
+	TREATMENT_REPORT,
+	DEATH_REPORT,
+	QUESTIONNAIRE_REPORT,
+	;
+}
