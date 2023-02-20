d5747d8a84590994489545037f63463b10344351
Shawn <shawn.r.hatch@gmail.com>
2023-02-20 14:59:03 -0500

    Implementing unit testing for TimeTrackingPolicy

--- gcm4/src/test/java/plugins/util/properties/AT_TimeTrackingPolicy.java
+++ gcm4/src/test/java/plugins/util/properties/AT_TimeTrackingPolicy.java
@@ -1,5 +1,7 @@
 package plugins.util.properties;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ public class AT_TimeTrackingPolicy {
 	@Test
 	@UnitTestMethod(target = TimeTrackingPolicy.class, name="next", args= {})
 	public void testNext() {
+		fail();
 		
 	}
 
@@ -17,7 +20,7 @@ public class AT_TimeTrackingPolicy {
 	@UnitTestMethod(target = TimeTrackingPolicy.class, name="getRandomTimeTrackingPolicy", args= {RandomGenerator.class})
 
 	public void testGetRandomTimeTrackingPolicy() {
-		
+		fail();
 	}
 	
 }
