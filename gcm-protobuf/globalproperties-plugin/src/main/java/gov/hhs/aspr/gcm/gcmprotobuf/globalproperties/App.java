924aff6a140523b4e9d979c17e197789dd110001
bischoffz <bischoffz@leidos.com>
2023-03-09 23:15:16 -0500

    convert global properties to use standard plugindata for test

--- gcm-protobuf/globalproperties-plugin/src/main/java/gov/hhs/aspr/gcm/gcmprotobuf/globalproperties/App.java
+++ gcm-protobuf/globalproperties-plugin/src/main/java/gov/hhs/aspr/gcm/gcmprotobuf/globalproperties/App.java
@@ -1,16 +1,20 @@
 package gov.hhs.aspr.gcm.gcmprotobuf.globalproperties;
 
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import gov.hhs.aspr.gcm.gcmprotobuf.core.TranslatorController;
+import gov.hhs.aspr.gcm.gcmprotobuf.globalproperties.translators.TestGlobalPropertyIdTranslator;
 import gov.hhs.aspr.gcm.gcmprotobuf.properties.PropertiesPluginBundle;
 import nucleus.PluginData;
 import plugins.globalproperties.GlobalPropertiesPluginData;
 import plugins.globalproperties.support.GlobalPropertyId;
+import plugins.globalproperties.testsupport.TestGlobalPropertyId;
 import plugins.util.properties.PropertyDefinition;
 
 public class App {
@@ -38,29 +42,52 @@ public class App {
         return target;
     }
 
+    private static boolean printNotSame() {
+        System.out.println("PluginDatas not same");
+        return false;
+    }
+
+    private static void checkSame(GlobalPropertiesPluginData actualPluginData) {
+        boolean isSame = true;
+        Set<TestGlobalPropertyId> expectedPropertyIds = EnumSet.allOf(TestGlobalPropertyId.class);
+
+        Set<GlobalPropertyId> actualGlobalPropertyIds = actualPluginData.getGlobalPropertyIds();
+        if (!expectedPropertyIds.equals(actualGlobalPropertyIds)) {
+            isSame = printNotSame();
+        }
+
+        for (TestGlobalPropertyId testGlobalPropertyId : TestGlobalPropertyId.values()) {
+            PropertyDefinition expectedPropertyDefinition = testGlobalPropertyId.getPropertyDefinition();
+            PropertyDefinition actualPropertyDefinition = actualPluginData
+                    .getGlobalPropertyDefinition(testGlobalPropertyId);
+
+            if (!expectedPropertyDefinition.equals(actualPropertyDefinition)) {
+                isSame = printNotSame();
+            }
+        }
+
+        if (isSame) {
+            System.out.println("PluginDatas are the same");
+        }
+    }
+
     public static void main(String[] args) {
 
-        String inputFileName = "./globalproperties-plugin/src/main/resources/json/testJson1.json";
-        String outputFileName = "./globalproperties-plugin/src/main/resources/json/output/testJson1Output.json";
+        String inputFileName = "./globalproperties-plugin/src/main/resources/json/testJson2.json";
+        String outputFileName = "./globalproperties-plugin/src/main/resources/json/output/testJson2Output.json";
 
         TranslatorController translatorController = TranslatorController.builder()
                 .addBundle(GlobalPropertiesPluginBundle.getPluginBundle(inputFileName, outputFileName))
                 .addBundle(PropertiesPluginBundle.getPluginBundle())
+                .addTranslator(new TestGlobalPropertyIdTranslator())
                 .build()
                 .init();
 
         List<PluginData> pluginDatas = translatorController.readInput().getPluginDatas();
 
-        GlobalPropertiesPluginData pluginData = (GlobalPropertiesPluginData) pluginDatas.get(0);
+        GlobalPropertiesPluginData actualPluginData = (GlobalPropertiesPluginData) pluginDatas.get(0);
 
-        System.out.println(pluginData.getGlobalPropertyIds());
-        for (GlobalPropertyId id : pluginData.getGlobalPropertyIds()) {
-            PropertyDefinition propertyDefinition = pluginData.getGlobalPropertyDefinition(id);
-            Object value = pluginData.getGlobalPropertyValue(id);
-            System.out.println(propertyDefinition);
-            System.out.println(value);
-
-        }
+        checkSame(actualPluginData);
 
         translatorController.writeOutput();
     }
