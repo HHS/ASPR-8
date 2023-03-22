f84f0894db231bbb535f4058ece1a6631f0ed7f1
bischoffz <bischoffz@leidos.com>
2023-03-22 00:49:00 -0400

    move app into test

--- translation/protobuf/globalproperties-plugin/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/globalproperties/AppTest.java
+++ translation/protobuf/globalproperties-plugin/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/globalproperties/AppTest.java
@@ -1,12 +1,12 @@
 package gov.hhs.aspr.gcm.translation.protobuf.plugins.globalproperties;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import org.junit.jupiter.api.Test;
 
 import gov.hhs.aspr.gcm.translation.protobuf.core.TranslatorController;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.globalproperties.translatorSpecs.TestGlobalPropertyIdTranslatorSpec;
@@ -17,62 +17,28 @@ import plugins.globalproperties.support.GlobalPropertyId;
 import plugins.globalproperties.testsupport.TestGlobalPropertyId;
 import plugins.util.properties.PropertyDefinition;
 
-public class App {
-
-    public JsonObject deepMerge(JsonObject source, JsonObject target) {
-        for (String key : source.keySet()) {
-            JsonElement value = source.get(key);
-            if (!target.has(key)) {
-                // new value for "key":
-                target.add(key, value);
-            } else {
-                // existing value for "key" - recursively deep merge:
-                if (value.isJsonObject()) {
-                    JsonObject valueJson = value.getAsJsonObject();
-                    deepMerge(valueJson, target.getAsJsonObject(key));
-                } else if (value.isJsonArray()) {
-                    JsonArray valueArray = value.getAsJsonArray();
-                    JsonArray targetArray = target.getAsJsonArray(key);
-                    targetArray.addAll(valueArray);
-                } else {
-                    target.add(key, value);
-                }
-            }
-        }
-        return target;
-    }
-
-    private static boolean printNotSame() {
-        System.out.println("PluginDatas not same");
-        return false;
-    }
+public class AppTest {
 
     private static void checkSame(GlobalPropertiesPluginData actualPluginData) {
-        boolean isSame = true;
+
         Set<TestGlobalPropertyId> expectedPropertyIds = EnumSet.allOf(TestGlobalPropertyId.class);
 
         Set<GlobalPropertyId> actualGlobalPropertyIds = actualPluginData.getGlobalPropertyIds();
-        if (!expectedPropertyIds.equals(actualGlobalPropertyIds)) {
-            isSame = printNotSame();
-        }
+        assertEquals(expectedPropertyIds, actualGlobalPropertyIds);
 
         for (TestGlobalPropertyId testGlobalPropertyId : TestGlobalPropertyId.values()) {
             PropertyDefinition expectedPropertyDefinition = testGlobalPropertyId.getPropertyDefinition();
             PropertyDefinition actualPropertyDefinition = actualPluginData
                     .getGlobalPropertyDefinition(testGlobalPropertyId);
 
-            if (!expectedPropertyDefinition.equals(actualPropertyDefinition)) {
-                isSame = printNotSame();
-            }
+            assertEquals(expectedPropertyDefinition, actualPropertyDefinition);
+
         }
 
-        if (isSame) {
-            System.out.println("PluginDatas are the same");
-        }
     }
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testGlobalPropertiesTranslator() {
         String inputFileName = "./globalproperties-plugin/src/main/resources/json/input.json";
         String outputFileName = "./globalproperties-plugin/src/main/resources/json/output/output.json";
 
