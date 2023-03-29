48d3bdec0131e720e552e060cdf6cd673aafc318
Baldwin-Akin Varner <akinvarner123@gmail.com>
2023-03-29 12:46:12 -0400

    Updated Group Property Report Test

--- gcm4/src/test/java/plugins/groups/reports/AT_GroupPropertyReport.java
+++ gcm4/src/test/java/plugins/groups/reports/AT_GroupPropertyReport.java
@@ -942,65 +942,6 @@ public class AT_GroupPropertyReport {
 
 		// Test without producing simulation
 
-		// add the action plugin
-		pluginBuilder = TestPluginData.builder();
-
-		// have the agent add new groups and set property values
-		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(0, (c) -> {
-			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
-
-			GroupId groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_1);
-			assertEquals(0, groupId.getValue());
-
-			groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_2);
-			assertEquals(1, groupId.getValue());
-
-			groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_3);
-			assertEquals(2, groupId.getValue());
-
-			groupId = groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_1);
-			assertEquals(3, groupId.getValue());
-
-			groupsDataManager.setGroupPropertyValue(new GroupId(3), TestGroupPropertyId.GROUP_PROPERTY_1_1_BOOLEAN_MUTABLE_NO_TRACK, true);
-
-			groupsDataManager.setGroupPropertyValue(new GroupId(3), TestGroupPropertyId.GROUP_PROPERTY_1_2_INTEGER_MUTABLE_NO_TRACK, 45);
-
-			groupsDataManager.setGroupPropertyValue(new GroupId(3), TestGroupPropertyId.GROUP_PROPERTY_1_3_DOUBLE_MUTABLE_NO_TRACK, 16.5);
-		}));
-
-		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(1, (c) -> {
-			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
-			groupsDataManager.removeGroup(new GroupId(0));
-			groupsDataManager.addGroup(TestGroupTypeId.GROUP_TYPE_2);
-		}));
-
-		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(2.5, (c) -> {
-			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
-			groupsDataManager.setGroupPropertyValue(new GroupId(1), TestGroupPropertyId.GROUP_PROPERTY_2_2_INTEGER_MUTABLE_TRACK, 17);
-			groupsDataManager.setGroupPropertyValue(new GroupId(4), TestGroupPropertyId.GROUP_PROPERTY_2_3_DOUBLE_MUTABLE_TRACK, 800.0);
-
-		}));
-
-		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(5.7, (c) -> {
-			GroupsDataManager groupsDataManager = c.getDataManager(GroupsDataManager.class);
-			groupsDataManager.setGroupPropertyValue(new GroupId(1), TestGroupPropertyId.GROUP_PROPERTY_2_1_BOOLEAN_MUTABLE_TRACK, false);
-			groupsDataManager.setGroupPropertyValue(new GroupId(4), TestGroupPropertyId.GROUP_PROPERTY_2_2_INTEGER_MUTABLE_TRACK, 65);
-		}));
-
-		testPluginData = pluginBuilder.build();
-
-		// build the report with all properties selected
-		builder = GroupPropertyReportPluginData.builder();
-		builder.setReportLabel(REPORT_LABEL)
-				.setReportPeriod(ReportPeriod.DAILY)
-				.setDefaultInclusion(true)
-				.excludeGroupProperty(TestGroupTypeId.GROUP_TYPE_2, TestGroupPropertyId.GROUP_PROPERTY_1_1_BOOLEAN_MUTABLE_NO_TRACK)
-				.includeGroupProperty(TestGroupTypeId.GROUP_TYPE_2, TestGroupPropertyId.GROUP_PROPERTY_2_2_INTEGER_MUTABLE_TRACK);
-
-		groupPropertyReportPluginData = builder.build();
-
-		factory = GroupsTestPluginFactory.factory(0, 0, 0, 6092832510476200219L, testPluginData).setGroupPropertyReportPluginData(groupPropertyReportPluginData);
-
 		testOutputConsumer = TestSimulation	.builder()//
 				.addPlugins(factory.getPlugins())//
 				.setProduceSimulationStateOnHalt(false)//
