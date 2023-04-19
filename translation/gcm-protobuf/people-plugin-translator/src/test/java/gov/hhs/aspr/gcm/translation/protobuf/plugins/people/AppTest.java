096885b7a93e86019384ea13cdac488dc6a851fd
bischoffz <bischoffz@leidos.com>
2023-04-19 13:23:38 -0400

    fix people test

--- translation/gcm-protobuf/people-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/people/AppTest.java
+++ translation/gcm-protobuf/people-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/people/AppTest.java
@@ -2,9 +2,7 @@ package gov.hhs.aspr.gcm.translation.protobuf.plugins.people;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.List;
 
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,48 +10,29 @@ import org.junit.jupiter.api.Test;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.people.input.PeoplePluginDataInput;
 import gov.hhs.aspr.translation.core.TranslatorController;
 import gov.hhs.aspr.translation.protobuf.core.ProtobufTranslatorCore;
+import gov.hhs.aspr.translation.protobuf.core.testsupport.TestResourceHelper;
 import plugins.people.PeoplePluginData;
-import plugins.people.support.PersonId;
 import plugins.people.support.PersonRange;
 import util.random.RandomGeneratorProvider;
 
 public class AppTest {
-    Path basePath = getCurrentDir();
-    Path inputFilePath = basePath.resolve("json");
-    Path outputFilePath = makeOutputDir();
+    Path basePath = TestResourceHelper.getResourceDir(this.getClass());
+    Path filePath = TestResourceHelper.makeTestOutputDir(basePath);
 
-    private Path getCurrentDir() {
-        try {
-            return Path.of(this.getClass().getClassLoader().getResource("").toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Path makeOutputDir() {
-        Path path = basePath.resolve("json/output");
-
-        path.toFile().mkdirs();
-
-        return path;
-    }
-    
     @Test
     public void testPeopleTranslator() {
         String fileName = "pluginData.json";
 
+        TestResourceHelper.createTestOutputFile(filePath, fileName);
+
         TranslatorController translatorController = TranslatorController.builder()
                 .setTranslatorCoreBuilder(ProtobufTranslatorCore.builder())
                 .addTranslator(PeopleTranslator.getTranslator())
-                .addInputFilePath(inputFilePath.resolve(fileName), PeoplePluginDataInput.class)
-                .addOutputFilePath(outputFilePath.resolve(fileName), PeoplePluginData.class)
+                .addInputFilePath(filePath.resolve(fileName), PeoplePluginDataInput.class)
+                .addOutputFilePath(filePath.resolve(fileName), PeoplePluginData.class)
                 .build();
 
-        translatorController.readInput();
-        PeoplePluginData actualPluginData = translatorController.getObject(PeoplePluginData.class);
-
         PeoplePluginData.Builder builder = PeoplePluginData.builder();
-
         RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(6573670690105604419L);
 
         int numRanges = randomGenerator.nextInt(15);
@@ -65,22 +44,11 @@ public class AppTest {
 
         PeoplePluginData expectedPluginData = builder.build();
 
-        assertEquals(expectedPluginData.getPersonCount(), actualPluginData.getPersonCount());
+        translatorController.writeOutput(expectedPluginData);
+        translatorController.readInput();
 
-        List<PersonRange> expectedRanges = expectedPluginData.getPersonRanges();
-        List<PersonRange> actualRanges = actualPluginData.getPersonRanges();
+        PeoplePluginData actualPluginData = translatorController.getObject(PeoplePluginData.class);
 
-        for (int i = 0; i < expectedRanges.size(); i++) {
-            assertEquals(expectedRanges.get(i), actualRanges.get(i));
-        }
-
-        List<PersonId> expectedPersonIds = expectedPluginData.getPersonIds();
-        List<PersonId> actualPersonIds = actualPluginData.getPersonIds();
-
-        for (int i = 0; i < expectedPersonIds.size(); i++) {
-            assertEquals(expectedPersonIds.get(i), actualPersonIds.get(i));
-        }
-
-        translatorController.writeOutput();
+        assertEquals(expectedPluginData, actualPluginData);
     }
 }
