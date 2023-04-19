b4e543c61cef322a0932cadc15c6d26e7a31e35a
bischoffz <bischoffz@leidos.com>
2023-04-19 03:49:48 -0400

    remove files

--- translation/gcm-protobuf/regions-plugin-translator/src/main/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/regions/RegionsTranslatorId.java
+++
@@ -1,8 +0,0 @@
-package gov.hhs.aspr.gcm.translation.protobuf.plugins.regions;
-
-import gov.hhs.aspr.translation.core.TranslatorId;
-
-public class RegionsTranslatorId implements TranslatorId {
-    public final static TranslatorId TRANSLATOR_ID = new RegionsTranslatorId();
-    private RegionsTranslatorId(){}
-}
