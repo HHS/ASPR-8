781796863d6d15d73d1c943d00cb42dd9295f310
bischoffz <bischoffz@leidos.com>
2023-04-19 01:51:34 -0400

    fix groups test

--- translation/gcm-protobuf/groups-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/groups/AppTest.java
+++ translation/gcm-protobuf/groups-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/groups/AppTest.java
@@ -1,16 +1,10 @@
 package gov.hhs.aspr.gcm.translation.protobuf.plugins.groups;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
@@ -21,65 +15,42 @@ import gov.hhs.aspr.gcm.translation.protobuf.plugins.people.PeopleTranslator;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.properties.PropertiesTranslator;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.reports.ReportsTranslator;
 import gov.hhs.aspr.translation.core.TranslatorController;
+import gov.hhs.aspr.translation.core.testsupport.TestResourceHelper;
 import gov.hhs.aspr.translation.protobuf.core.ProtobufTranslatorCore;
 import plugins.groups.GroupsPluginData;
 import plugins.groups.reports.GroupPropertyReportPluginData;
-import plugins.groups.support.GroupId;
-import plugins.groups.support.GroupPropertyValue;
-import plugins.groups.support.GroupTypeId;
+import plugins.groups.testsupport.GroupsTestPluginFactory;
 import plugins.groups.testsupport.TestGroupPropertyId;
 import plugins.groups.testsupport.TestGroupTypeId;
 import plugins.people.support.PersonId;
 import plugins.reports.support.ReportLabel;
 import plugins.reports.support.ReportPeriod;
 import plugins.reports.support.SimpleReportLabel;
-import plugins.util.properties.PropertyDefinition;
 import util.random.RandomGeneratorProvider;
-import util.wrappers.MultiKey;
 
 public class AppTest {
-    Path basePath = getCurrentDir();
-    Path inputFilePath = basePath.resolve("json");
-    Path outputFilePath = makeOutputDir();
-
-    private Path getCurrentDir() {
-        try {
-            return Path.of(this.getClass().getClassLoader().getResource("").toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Path makeOutputDir() {
-        Path path = basePath.resolve("json/output");
-
-        path.toFile().mkdirs();
-
-        return path;
-    }
+    Path basePath = TestResourceHelper.getResourceDir(this.getClass());
+    Path filePath = TestResourceHelper.makeTestOutputDir(basePath);
 
     @Test
     public void testGroupsTranslator() {
         String fileName = "pluginData.json";
+
+        TestResourceHelper.createTestOutputFile(filePath, fileName);
 
         TranslatorController translatorController = TranslatorController.builder()
                 .setTranslatorCoreBuilder(ProtobufTranslatorCore.builder())
                 .addTranslator(GroupsTranslator.getTranslator())
                 .addTranslator(PropertiesTranslator.getTranslator())
                 .addTranslator(PeopleTranslator.getTranslator())
-                .addInputFilePath(inputFilePath.resolve(fileName), GroupsPluginDataInput.class)
-                .addOutputFilePath(outputFilePath.resolve(fileName), GroupsPluginData.class)
+                .addInputFilePath(filePath.resolve(fileName), GroupsPluginDataInput.class)
+                .addOutputFilePath(filePath.resolve(fileName), GroupsPluginData.class)
                 .build();
-
-        translatorController.readInput();
-
-        GroupsPluginData groupsPluginData = translatorController.getObject(GroupsPluginData.class);
 
         long seed = 524805676405822016L;
         int initialPopulation = 100;
         int groupCount = 5;
         int membershipCount = 100;
-        int expectedGroupsPerPerson = 1;
 
         List<PersonId> people = new ArrayList<>();
 
@@ -87,84 +58,22 @@ public class AppTest {
             people.add(new PersonId(i));
         }
 
-        Set<TestGroupTypeId> expectedGroupTypeIds = EnumSet.allOf(TestGroupTypeId.class);
-        assertFalse(expectedGroupTypeIds.isEmpty());
+        GroupsPluginData expectedPluginData = GroupsTestPluginFactory.getStandardGroupsPluginData(groupCount,
+                membershipCount, people, seed);
+        translatorController.writeOutput(expectedPluginData);
 
-        Set<GroupTypeId> actualGroupTypeIds = groupsPluginData.getGroupTypeIds();
-        assertEquals(expectedGroupTypeIds, actualGroupTypeIds);
+        translatorController.readInput();
 
-        Set<TestGroupPropertyId> expectedGroupPropertyIds = EnumSet.allOf(TestGroupPropertyId.class);
-        assertFalse(expectedGroupPropertyIds.isEmpty());
+        GroupsPluginData actualPluginData = translatorController.getObject(GroupsPluginData.class);
 
-        for (TestGroupPropertyId expectedPropertyId : TestGroupPropertyId.values()) {
-            TestGroupTypeId expectedGroupTypeId = expectedPropertyId.getTestGroupTypeId();
-            PropertyDefinition expectedPropertyDefinition = expectedPropertyId.getPropertyDefinition();
-
-            assertTrue(groupsPluginData.getGroupPropertyIds(expectedGroupTypeId).contains(expectedPropertyId));
-            PropertyDefinition actualPropertyDefinition = groupsPluginData
-                    .getGroupPropertyDefinition(expectedGroupTypeId, expectedPropertyId);
-            assertEquals(expectedPropertyDefinition, actualPropertyDefinition);
-        }
-
-        assertEquals(groupCount, groupsPluginData.getGroupIds().size());
-        assertEquals(initialPopulation, groupsPluginData.getPersonCount());
-
-        RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(seed);
-        for (GroupId groupId : groupsPluginData.getGroupIds()) {
-            GroupTypeId actualGroupTypeId = groupsPluginData.getGroupTypeId(groupId);
-            TestGroupTypeId expectedGroupTypeId = TestGroupTypeId.getRandomGroupTypeId(randomGenerator);
-            assertEquals(expectedGroupTypeId, actualGroupTypeId);
-
-            List<GroupPropertyValue> expectedGroupPropertyValues = new ArrayList<>();
-            for (TestGroupPropertyId testGroupPropertyId : TestGroupPropertyId
-                    .getTestGroupPropertyIds(expectedGroupTypeId)) {
-                GroupPropertyValue expectedValue = new GroupPropertyValue(testGroupPropertyId,
-                        testGroupPropertyId.getRandomPropertyValue(randomGenerator));
-                expectedGroupPropertyValues.add(expectedValue);
-            }
-
-            assertEquals(expectedGroupPropertyValues.size(), groupsPluginData.getGroupPropertyValues(groupId).size());
-            for (int i = 0; i < expectedGroupPropertyValues.size(); i++) {
-                assertEquals(expectedGroupPropertyValues.get(i),
-                        groupsPluginData.getGroupPropertyValues(groupId).get(i));
-            }
-            expectedGroupTypeId = expectedGroupTypeId.next();
-        }
-
-        Set<MultiKey> groupMemeberships = new LinkedHashSet<>();
-        List<GroupId> groups = groupsPluginData.getGroupIds();
-        while (groupMemeberships.size() < membershipCount) {
-            PersonId personId = people.get(randomGenerator.nextInt(people.size()));
-            GroupId groupId = groups.get(randomGenerator.nextInt(groups.size()));
-            groupMemeberships.add(new MultiKey(groupId, personId));
-        }
-
-        for (MultiKey multiKey : groupMemeberships) {
-            GroupId expectedGroupId = multiKey.getKey(0);
-            PersonId expectedPersonId = multiKey.getKey(1);
-
-            assertTrue(groupsPluginData.getGroupsForPerson(expectedPersonId).contains(expectedGroupId));
-        }
-
-        double numGroups = 0;
-        for (PersonId person : people) {
-            numGroups += groupsPluginData.getGroupsForPerson(person).size();
-        }
-
-        double actualGroupsPerPerson = numGroups / initialPopulation;
-
-        double lowerBound = expectedGroupsPerPerson * 0.9;
-        double upperBound = expectedGroupsPerPerson * 1.1;
-
-        assertTrue(actualGroupsPerPerson <= upperBound);
-        assertTrue(actualGroupsPerPerson > lowerBound);
-
-        translatorController.writeOutput();
+        assertEquals(expectedPluginData, actualPluginData);
     }
 
     @Test
     public void testGroupPropertyReportTranslatorSpec() {
         String fileName = "propertyReport.json";
+
+        TestResourceHelper.createTestOutputFile(filePath, fileName);
 
         TranslatorController translatorController = TranslatorController.builder()
                 .setTranslatorCoreBuilder(ProtobufTranslatorCore.builder())
@@ -172,14 +81,9 @@ public class AppTest {
                 .addTranslator(PropertiesTranslator.getTranslator())
                 .addTranslator(PeopleTranslator.getTranslator())
                 .addTranslator(ReportsTranslator.getTranslator())
-                .addInputFilePath(inputFilePath.resolve(fileName), GroupPropertyReportPluginDataInput.class)
-                .addOutputFilePath(outputFilePath.resolve(fileName), GroupPropertyReportPluginData.class)
+                .addInputFilePath(filePath.resolve(fileName), GroupPropertyReportPluginDataInput.class)
+                .addOutputFilePath(filePath.resolve(fileName), GroupPropertyReportPluginData.class)
                 .build();
-
-        translatorController.readInput();
-
-        GroupPropertyReportPluginData actualPluginData = translatorController
-                .getObject(GroupPropertyReportPluginData.class);
 
         RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(524805676405822016L);
 
@@ -201,18 +105,14 @@ public class AppTest {
 
         GroupPropertyReportPluginData expectedPluginData = builder.build();
 
-        assertEquals(expectedPluginData.getReportLabel(), actualPluginData.getReportLabel());
-        assertEquals(expectedPluginData.getReportPeriod(), actualPluginData.getReportPeriod());
-        assertEquals(expectedPluginData.getDefaultInclusionPolicy(), actualPluginData.getDefaultInclusionPolicy());
-        assertEquals(expectedPluginData.getGroupTypeIds(), actualPluginData.getGroupTypeIds());
+        translatorController.writeOutput(expectedPluginData);
 
-        for (GroupTypeId groupTypeId : actualPluginData.getGroupTypeIds()) {
-            assertEquals(expectedPluginData.getIncludedProperties(groupTypeId),
-                    actualPluginData.getIncludedProperties(groupTypeId));
-            assertEquals(expectedPluginData.getExcludedProperties(groupTypeId),
-                    actualPluginData.getExcludedProperties(groupTypeId));
-        }
+        translatorController.readInput();
 
-        translatorController.writeOutput();
+        GroupPropertyReportPluginData actualPluginData = translatorController
+                .getObject(GroupPropertyReportPluginData.class);
+
+        assertEquals(expectedPluginData, actualPluginData);
+
     }
 }
