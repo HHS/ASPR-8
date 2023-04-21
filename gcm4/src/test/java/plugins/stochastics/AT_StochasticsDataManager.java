291b0fd3c8d461091cc4bafa3684338af5870e10
Baldwin-Akin Varner <akinvarner123@gmail.com>
2023-04-21 18:04:01 -0400

    Updated test

--- gcm4/src/test/java/plugins/stochastics/AT_StochasticsDataManager.java
+++ gcm4/src/test/java/plugins/stochastics/AT_StochasticsDataManager.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
+import nucleus.testsupport.testplugin.TestActorPlan;
+import nucleus.testsupport.testplugin.TestOutputConsumer;
+import nucleus.testsupport.testplugin.TestPluginData;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +16,15 @@ import nucleus.DataManagerContext;
 import nucleus.testsupport.testplugin.TestSimulation;
 import plugins.stochastics.support.RandomNumberGeneratorId;
 import plugins.stochastics.support.StochasticsError;
+import plugins.stochastics.support.Well;
+import plugins.stochastics.support.WellState;
 import plugins.stochastics.testsupport.StochasticsTestPluginFactory;
 import plugins.stochastics.testsupport.StochasticsTestPluginFactory.Factory;
 import plugins.stochastics.testsupport.TestRandomGeneratorId;
 import util.annotations.UnitTestConstructor;
 import util.annotations.UnitTestMethod;
 import util.errors.ContractException;
+import util.random.RandomGeneratorProvider;
 
 public class AT_StochasticsDataManager {
 
@@ -27,6 +32,45 @@ public class AT_StochasticsDataManager {
 	@UnitTestMethod(target = StochasticsDataManager.class, name = "init", args = { DataManagerContext.class })
 	public void testInit() {
 		// nothing to test
+	}
+
+	@Test
+	@UnitTestMethod(target = StochasticsDataManager.class, name = "init", args = {DataManagerContext.class})
+	public void testInit_State() {
+		WellState wellState = WellState.builder().build();
+		StochasticsPluginData stochasticsPluginData = StochasticsPluginData.builder()
+				.setMainRNGState(wellState)
+				.addRNG(TestRandomGeneratorId.BLITZEN, wellState)
+				.build();
+		List<RandomNumberGeneratorId> expectedRandomGeneratorIds = new ArrayList<>();
+		List<WellState> expectedWellStates = new ArrayList<>();
+		TestPluginData.Builder pluginBuilder = TestPluginData.builder();
+
+		pluginBuilder.addTestActorPlan("actor", new TestActorPlan(0, (c) -> {
+			StochasticsDataManager stochasticsDataManager = c.getDataManager(StochasticsDataManager.class);
+//			WellState wellState = WellState.builder().build();
+//			stochasticsDataManager.addRandomNumberGenerator(TestRandomGeneratorId.BLITZEN, wellState);
+//			expectedRandomGeneratorIds.add(TestRandomGeneratorId.BLITZEN);
+//			expectedWellStates.add(wellState);
+		}));
+
+		TestPluginData testPluginData = pluginBuilder.build();
+		Factory factory = StochasticsTestPluginFactory.factory(3078336459131759089L, testPluginData)
+				.setStochasticsPluginData(stochasticsPluginData);
+		TestOutputConsumer testOutputConsumer = TestSimulation.builder().addPlugins(factory.getPlugins())
+				.setProduceSimulationStateOnHalt(true)
+				.setSimulationHaltTime(2)
+				.build()
+				.execute();
+		Map<StochasticsPluginData, Integer> outputItems = testOutputConsumer.getOutputItems(StochasticsPluginData.class);
+		assertEquals(1, outputItems.size());
+		StochasticsPluginData actualPluginData = outputItems.keySet().iterator().next();
+		StochasticsPluginData expectedPluginData = StochasticsPluginData.builder()
+				.setMainRNGState(wellState)
+				.addRNG(TestRandomGeneratorId.BLITZEN, wellState)
+				.build();
+		assertEquals(expectedPluginData, actualPluginData);
+
 	}
 
 	@Test
