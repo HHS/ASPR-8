cec3d306aa1d206fbfb29b4368a804fcce02fca2
bischoffz <bischoffz@leidos.com>
2023-05-10 03:28:33 -0400

    format

--- translation/protobuf-translation-core/src/test/java/gov/hhs/aspr/translation/protobuf/core/AT_ProtobufCoreTranslationError.java
+++ translation/protobuf-translation-core/src/test/java/gov/hhs/aspr/translation/protobuf/core/AT_ProtobufCoreTranslationError.java
@@ -11,21 +11,21 @@ import org.junit.jupiter.api.Test;
 import util.annotations.UnitTestMethod;
 
 public class AT_ProtobufCoreTranslationError {
-    
-    @Test
-	@UnitTestMethod(target = ProtobufCoreTranslationError.class, name = "getDescription", args = {})
-	public void testGetDescription() {
-		// show that each ErrorType has a non-null, non-empty description
-		for (ProtobufCoreTranslationError coreTranslationError : ProtobufCoreTranslationError.values()) {
-			assertNotNull(coreTranslationError.getDescription());
-			assertTrue(coreTranslationError.getDescription().length() > 0);
-		}
 
-		// show that each description is unique (ignoring case as well)
-		Set<String> descriptions = new LinkedHashSet<>();
-		for (ProtobufCoreTranslationError coreTranslationError : ProtobufCoreTranslationError.values()) {
-			boolean isUnique = descriptions.add(coreTranslationError.getDescription().toLowerCase());
-			assertTrue(isUnique, coreTranslationError + " duplicates the description of another member");
-		}
-	}
+    @Test
+    @UnitTestMethod(target = ProtobufCoreTranslationError.class, name = "getDescription", args = {})
+    public void testGetDescription() {
+        // show that each ErrorType has a non-null, non-empty description
+        for (ProtobufCoreTranslationError coreTranslationError : ProtobufCoreTranslationError.values()) {
+            assertNotNull(coreTranslationError.getDescription());
+            assertTrue(coreTranslationError.getDescription().length() > 0);
+        }
+
+        // show that each description is unique (ignoring case as well)
+        Set<String> descriptions = new LinkedHashSet<>();
+        for (ProtobufCoreTranslationError coreTranslationError : ProtobufCoreTranslationError.values()) {
+            boolean isUnique = descriptions.add(coreTranslationError.getDescription().toLowerCase());
+            assertTrue(isUnique, coreTranslationError + " duplicates the description of another member");
+        }
+    }
 }
