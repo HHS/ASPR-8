e106c67e7dea7a5b0f2888851218334d1752f5df
bischoffz <bischoffz@leidos.com>
2023-06-01 02:13:29 -0400

    add group filter translationSpecs

---
+++ translation/gcm-protobuf-translation/groups-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/groups/translationSpecs/AT_GroupMemberFilterTranslationSpec.java
@@ -0,0 +1,70 @@
+package gov.hhs.aspr.gcm.translation.protobuf.plugins.groups.translationSpecs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import gov.hhs.aspr.gcm.translation.protobuf.plugins.groups.GroupsTranslator;
+import gov.hhs.aspr.gcm.translation.protobuf.plugins.groups.input.GroupMemberFilterInput;
+import gov.hhs.aspr.gcm.translation.protobuf.plugins.partitions.PartitionsTranslator;
+import gov.hhs.aspr.gcm.translation.protobuf.plugins.people.PeopleTranslator;
+import gov.hhs.aspr.gcm.translation.protobuf.plugins.properties.PropertiesTranslator;
+import gov.hhs.aspr.translation.core.TranslationController;
+import gov.hhs.aspr.translation.protobuf.core.ProtobufTranslationEngine;
+import plugins.groups.support.GroupId;
+import plugins.groups.support.GroupMemberFilter;
+import util.annotations.UnitTestConstructor;
+import util.annotations.UnitTestForCoverage;
+import util.annotations.UnitTestMethod;
+
+public class AT_GroupMemberFilterTranslationSpec {
+
+    @Test
+    @UnitTestConstructor(target = GroupMemberFilterTranslationSpec.class, args = {})
+    public void testConstructor() {
+        assertNotNull(new GroupMemberFilterTranslationSpec());
+    }
+
+    @Test
+    @UnitTestForCoverage
+    public void testConvertObject() {
+        TranslationController translationController = TranslationController.builder()
+                .setTranslationEngineBuilder(ProtobufTranslationEngine.builder())
+                .addTranslator(GroupsTranslator.getTranslator())
+                .addTranslator(PropertiesTranslator.getTranslator())
+                .addTranslator(PeopleTranslator.getTranslator())
+                .addTranslator(PartitionsTranslator.getTranslator())
+                .build();
+
+        ProtobufTranslationEngine protobufTranslationEngine = translationController
+                .getTranslationEngine(ProtobufTranslationEngine.class);
+
+        GroupMemberFilterTranslationSpec translationSpec = new GroupMemberFilterTranslationSpec();
+        translationSpec.init(protobufTranslationEngine);
+
+        GroupMemberFilter expectedAppValue = new GroupMemberFilter(new GroupId(0));
+
+        GroupMemberFilterInput inputValue = translationSpec.convertAppObject(expectedAppValue);
+
+        GroupMemberFilter actualAppValue = translationSpec.convertInputObject(inputValue);
+
+        assertEquals(expectedAppValue, actualAppValue);
+    }
+
+    @Test
+    @UnitTestMethod(target = GroupMemberFilterTranslationSpec.class, name = "getAppObjectClass", args = {})
+    public void testGetAppObjectClass() {
+        GroupMemberFilterTranslationSpec translationSpec = new GroupMemberFilterTranslationSpec();
+
+        assertEquals(GroupMemberFilter.class, translationSpec.getAppObjectClass());
+    }
+
+    @Test
+    @UnitTestMethod(target = GroupMemberFilterTranslationSpec.class, name = "getInputObjectClass", args = {})
+    public void testGetInputObjectClass() {
+        GroupMemberFilterTranslationSpec translationSpec = new GroupMemberFilterTranslationSpec();
+
+        assertEquals(GroupMemberFilterInput.class, translationSpec.getInputObjectClass());
+    }
+}
