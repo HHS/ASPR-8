5d5b514639427a9a49eb95cfdfa2878ec9a6289e
bischoffz <bischoffz@leidos.com>
2023-06-08 12:58:38 -0400

    update person props translator

--- translation/gcm-protobuf-translation/personproperties-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/personproperties/AT_PersonPropertiesTranslator.java
+++ translation/gcm-protobuf-translation/personproperties-plugin-translator/src/test/java/gov/hhs/aspr/gcm/translation/protobuf/plugins/personproperties/AT_PersonPropertiesTranslator.java
@@ -1,71 +1,73 @@
 package gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.people.PeopleTranslatorId;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.input.PersonPropertyValueInput;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.translationSpecs.PersonPropertiesPluginDataTranslationSpec;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.translationSpecs.PersonPropertyIdTranslationSpec;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.translationSpecs.PersonPropertyInteractionReportPluginDataTranslationSpec;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.translationSpecs.PersonPropertyReportPluginDataTranslationSpec;
-import gov.hhs.aspr.gcm.translation.protobuf.plugins.personproperties.translationSpecs.TestPersonPropertyIdTranslationSpec;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.properties.PropertiesTranslatorId;
 import gov.hhs.aspr.gcm.translation.protobuf.plugins.reports.ReportsTranslatorId;
+import gov.hhs.aspr.translation.core.TranslationSpec;
 import gov.hhs.aspr.translation.core.Translator;
-import gov.hhs.aspr.translation.protobuf.core.ProtobufTranslationEngine;
+import util.annotations.UnitTestForCoverage;
 import util.annotations.UnitTestMethod;
 
 public class AT_PersonPropertiesTranslator {
 
-    @Test
-    @UnitTestMethod(target = PersonPropertiesTranslator.class, name = "getTranslator", args = {})
-    public void testGetTranslator() {
-        Translator expectedTranslator = Translator.builder()
-                .setTranslatorId(PersonPropertiesTranslatorId.TRANSLATOR_ID)
-                .addDependency(PropertiesTranslatorId.TRANSLATOR_ID)
-                .addDependency(PeopleTranslatorId.TRANSLATOR_ID)
-                .setInitializer((translatorContext) -> {
-                    ProtobufTranslationEngine.Builder translationEngineBuilder = translatorContext
-                            .getTranslationEngineBuilder(ProtobufTranslationEngine.Builder.class);
+        @Test
+        @UnitTestForCoverage
+        public void testGetTranslationSpecs() throws ClassNotFoundException {
+                List<TranslationSpec<?, ?>> translationSpecs = PersonPropertiesTranslator.getTranslationSpecs();
+                List<Class<?>> translationSpecClasses = new ArrayList<>();
 
-                    translationEngineBuilder.addTranslationSpec(new PersonPropertyIdTranslationSpec())
-                            .addTranslationSpec(new PersonPropertiesPluginDataTranslationSpec())
-                            .addTranslationSpec(new TestPersonPropertyIdTranslationSpec());
+                for (TranslationSpec<?, ?> translationSpec : translationSpecs) {
+                        translationSpecClasses.add(translationSpec.getClass());
+                }
 
-                    translationEngineBuilder.addFieldToIncludeDefaultValue(
-                            PersonPropertyValueInput.getDescriptor().findFieldByName("personId"));
+                String packageName = this.getClass().getPackageName() + ".translationSpecs";
+                ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+                URL packageURL;
 
-                }).build();
+                packageURL = classLoader.getResource(packageName);
 
-        assertEquals(expectedTranslator, PersonPropertiesTranslator.getTranslator());
-    }
+                if (packageURL != null) {
+                        String packagePath = packageURL.getPath();
+                        if (packagePath != null) {
+                                File packageDir = new File(packagePath);
+                                if (packageDir.isDirectory()) {
+                                        File[] files = packageDir.listFiles();
+                                        for (File file : files) {
+                                                String className = file.getName();
+                                                if (className.endsWith(".class")) {
+                                                        className = packageName + "." + className.substring(0,
+                                                                        className.length() - 6);
+                                                        assertTrue(translationSpecClasses
+                                                                        .contains(classLoader.loadClass(className)));
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
 
-    @Test
-    @UnitTestMethod(target = PersonPropertiesTranslator.class, name = "getTranslatorWithReport", args = {})
-    public void testGetTranslatorWithReport() {
-        Translator expectedTranslator = Translator.builder()
-                .setTranslatorId(PersonPropertiesTranslatorId.TRANSLATOR_ID)
-                .addDependency(PropertiesTranslatorId.TRANSLATOR_ID)
-                .addDependency(PeopleTranslatorId.TRANSLATOR_ID)
-                .addDependency(ReportsTranslatorId.TRANSLATOR_ID)
-                .setInitializer((translatorContext) -> {
-                    ProtobufTranslationEngine.Builder translationEngineBuilder = translatorContext
-                            .getTranslationEngineBuilder(ProtobufTranslationEngine.Builder.class);
+        @Test
+        @UnitTestMethod(target = PersonPropertiesTranslator.class, name = "getTranslator", args = {})
+        public void testGetTranslator() {
+                Translator expectedTranslator = Translator.builder()
+                                .setTranslatorId(PersonPropertiesTranslatorId.TRANSLATOR_ID)
+                                .addDependency(PropertiesTranslatorId.TRANSLATOR_ID)
+                                .addDependency(PeopleTranslatorId.TRANSLATOR_ID)
+                                .addDependency(ReportsTranslatorId.TRANSLATOR_ID)
+                                .setInitializer((translatorContext) -> {
+                                }).build();
 
-                    translationEngineBuilder
-                            .addTranslationSpec(new PersonPropertyIdTranslationSpec())
-                            .addTranslationSpec(new PersonPropertiesPluginDataTranslationSpec())
-                            .addTranslationSpec(new PersonPropertyReportPluginDataTranslationSpec())
-                            .addTranslationSpec(new PersonPropertyInteractionReportPluginDataTranslationSpec())
-                            .addTranslationSpec(new TestPersonPropertyIdTranslationSpec());
+                assertEquals(expectedTranslator, PersonPropertiesTranslator.getTranslator());
+        }
 
-                    translationEngineBuilder.addFieldToIncludeDefaultValue(
-                            PersonPropertyValueInput.getDescriptor().findFieldByName("personId"));
-
-                }).build();
-
-        assertEquals(expectedTranslator, PersonPropertiesTranslator.getTranslatorWithReport());
-    }
 }
