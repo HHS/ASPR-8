f1954f909970b31ac0cca224182e926fc9c3f5d9
shawnhatch <30730734+shawnhatch@users.noreply.github.com>
2023-08-23 15:04:53 -0400

    Fixed documentation typo. (#181)
    Co-authored-by: Shawn <shawn.r.hatch@gmail.com>

--- tutorials/lessons/lesson_06_plugin_dependencies/src/main/java/gov/hhs/aspr/ms/gcm/lessons/Example_6.java
+++ tutorials/lessons/lesson_06_plugin_dependencies/src/main/java/gov/hhs/aspr/ms/gcm/lessons/Example_6.java
@@ -17,7 +17,7 @@ public final class Example_6 {
 	/*
 	 * start code_ref=plugin_dependencies_connecting_the_plugins|code_cap=The
 	 * people, vaccine, family and model plugins are contributed to the simulation.
-	 * On execution, the model pluin's single actor schedules the vaccination of
+	 * On execution, the model plugin's single actor schedules the vaccination of
 	 * each person as well as a few random removals of people from the simulation.
 	 */
 	public static void main(String[] args) {
