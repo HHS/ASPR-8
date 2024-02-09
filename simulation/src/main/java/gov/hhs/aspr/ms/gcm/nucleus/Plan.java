ebbb5c4b8479335c7d999bc1ac2edaa256dde590
bischoffz <bischoffz@leidos.com>
2024-02-08 23:54:40 -0500

    refactor plan

--- simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/Plan.java
+++ simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/Plan.java
@@ -1,114 +1,33 @@
 package gov.hhs.aspr.ms.gcm.nucleus;
 
-import java.util.function.Consumer;
+public abstract class Plan {
+	private double time;
+	private boolean active = true;
+	long arrivalId = -1;
 
-public class Plan<T> {
-
-	private static class Data<K> {
-		private double time;
-		private Consumer<K> callbackConsumer;
-		private boolean active = true;
-
-		public Data() {
-		}
-
-		public Data(Data<K> data) {
-			time = data.time;
-			callbackConsumer = data.callbackConsumer;
-			active = data.active;
-		}
-
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("Data [time=");
-			builder.append(time);
-			builder.append(", callbackConsumer=");
-			builder.append(callbackConsumer);
-			builder.append(", active=");
-			builder.append(active);
-			builder.append("]");
-			return builder.toString();
-		}
-
+	protected Plan(double time) {
+		this.time = time;
 	}
 
-	/**
-	 * Returns a new instance of the Builder class
-	 */
-	public static <K> Builder<K> builder(Class<K> classReference) {
-		return new Builder<K>();
+	protected Plan(double time, boolean active) {
+		this.time = time;
+		this.active = active;
 	}
 
-	public static class Builder<K> {
-		private Data<K> data = new Data<>();
-
-		private Builder() {
-
-		}
-
-		/**
-		 * Constructs a new EventLabel from the collected data.
-		 */
-		public Plan<K> build() {
-			return new Plan<>(new Data<>(data));
-		}
-
-		/**
-		 * Sets the time of the plan.
-		 */
-		public Builder<K> setTime(double time) {
-			data.time = time;
-			return this;
-		}
-
-		/**
-		 * Sets the active state for the plan. The planning queue continues execution
-		 * while there are active plans present. Passive plans should be used for
-		 * recurring, non-event driven tasks that do not require the continued execution
-		 * of the simulation.Defaults to true;
-		 */
-		public Builder<K> setActive(boolean active) {
-			data.active = active;
-			return this;
-		}
-
-		/**
-		 * Sets the required call back behavior for this plan. The call back is executed
-		 * when the plan reaches the top of the queue and the simulation's time is set
-		 * to the plan's time. No default is allowed.
-		 */
-		public Builder<K> setCallbackConsumer(Consumer<K> callbackConsumer) {
-			data.callbackConsumer = callbackConsumer;
-			return this;
-		}
+	public final double getTime() {
+		return time;
 	}
 
-	private final Data<T> data;
-
-	private Plan(Data<T> data) {
-		this.data = data;
+	public final boolean isActive() {
+		return active;
 	}
 
-	public double getTime() {
-		return data.time;
+	public final void setActive(boolean active) {
+		this.active = active;
 	}
 
-	public Consumer<T> getCallbackConsumer() {
-		return data.callbackConsumer;
-	}
-
-	public boolean isActive() {
-		return data.active;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder2 = new StringBuilder();
-		builder2.append("Plan [data=");
-		builder2.append(data);
-		builder2.append("]");
-		return builder2.toString();
+	public final long getArrivalId() {
+		return arrivalId;
 	}
 
 }
