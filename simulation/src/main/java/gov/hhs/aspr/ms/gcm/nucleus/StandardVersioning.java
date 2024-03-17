60b8d39e6791b3ccad0f1366d31b73ad7e9bf358
bischoffz <bischoffz@leidos.com>
2024-03-16 23:36:40 -0400

    make member var private

--- simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/StandardVersioning.java
+++ simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/StandardVersioning.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class StandardVersioning {
     public static final String VERSION = "4.2.0";
 
-    public static final List<String> supportedVersions = Arrays.asList("", "4.0.0", "4.1.0", VERSION);
+    private static final List<String> supportedVersions = Arrays.asList("", "4.0.0", "4.1.0", VERSION);
 
     public static boolean checkVersionSupported(String version) {
         return supportedVersions.contains(version);
