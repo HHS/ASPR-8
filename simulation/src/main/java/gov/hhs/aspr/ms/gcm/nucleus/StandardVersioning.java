c97ebf27bcb0e6e11949b439c8b010380d2e7685
bischoffz <bischoffz@leidos.com>
2024-03-18 17:42:16 -0400

    make constructor private

--- simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/StandardVersioning.java
+++ simulation/src/main/java/gov/hhs/aspr/ms/gcm/nucleus/StandardVersioning.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StandardVersioning {
+    private StandardVersioning() {
+    }
+
     public static final String VERSION = "4.2.0";
 
     private static final List<String> supportedVersions = Arrays.asList("", "4.0.0", "4.1.0", VERSION);
