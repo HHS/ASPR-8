13f98a854c8a2cebb57f9f763c55547701022e3a
Zachary Bischoff <116595361+bischoffz@users.noreply.github.com>
2024-03-25 17:27:35 -0400

    bring pipeline interfaces and error from epifast (#220)

---
+++ pipeline/src/main/java/gov/hhs/aspr/ms/gcm/pipeline/IPipeline.java
@@ -0,0 +1,5 @@
+package gov.hhs.aspr.ms.epifast.pipeline.pipelines;
+
+public interface IPipeline {
+    void execute();
+}
---
+++ pipeline/src/test/java/gov/hhs/aspr/ms/gcm/pipeline/support/AT_PipelineError.java
@@ -0,0 +1,28 @@
+package gov.hhs.aspr.ms.epifast.pipeline.support;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import gov.hhs.aspr.ms.util.annotations.UnitTestMethod;
+
+public class AT_PipelineError {
+    
+    @Test
+    @UnitTestMethod(target = PipelineError.class, name = "getDescription", args = {})
+    public void testGetDescription() {
+        // show that each description is a unique, non-null and non-empty string
+        Set<String> descriptions = new LinkedHashSet<>();
+        for (PipelineError personBlockError : PipelineError.values()) {
+            String description = personBlockError.getDescription();
+            assertNotNull(description, "null description for " + personBlockError);
+            assertTrue(description.length() > 0, "empty string for " + personBlockError);
+            boolean unique = descriptions.add(description);
+            assertTrue(unique, "description for " + personBlockError + " is not unique");
+        }
+    }
+}
