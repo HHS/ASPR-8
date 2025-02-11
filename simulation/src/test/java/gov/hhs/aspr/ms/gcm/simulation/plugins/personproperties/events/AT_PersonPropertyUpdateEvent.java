7dbbed7b0b82c021174892dbf6881fc368a61c04
Riegler <rieglert@leidos.com>
2025-02-10 20:03:31 -0500

    Removed record-based unit tests based on MetaInfoReport

--- simulation/src/test/java/gov/hhs/aspr/ms/gcm/simulation/plugins/personproperties/events/AT_PersonPropertyUpdateEvent.java
+++ simulation/src/test/java/gov/hhs/aspr/ms/gcm/simulation/plugins/personproperties/events/AT_PersonPropertyUpdateEvent.java
@@ -7,60 +7,10 @@ import org.junit.jupiter.api.Test;
 import gov.hhs.aspr.ms.gcm.simulation.plugins.people.support.PersonId;
 import gov.hhs.aspr.ms.gcm.simulation.plugins.personproperties.support.PersonPropertyId;
 import gov.hhs.aspr.ms.gcm.simulation.plugins.personproperties.testsupport.TestPersonPropertyId;
-import gov.hhs.aspr.ms.util.annotations.UnitTestConstructor;
 import gov.hhs.aspr.ms.util.annotations.UnitTestMethod;
 
 public class AT_PersonPropertyUpdateEvent {
 
-	@Test
-	@UnitTestConstructor(target = PersonPropertyUpdateEvent.class, args = { PersonId.class, PersonPropertyId.class, Object.class, Object.class })
-	public void testConstructor() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "equals", args = { Object.class })
-	public void testEquals() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "toString", args = {})
-	public void testToString() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "hashCode", args = {})
-	public void testHashCode() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "personId", args = {})
-	public void testPersonId() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "personPropertyId", args = {})
-	public void testPersonPropertyId() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "previousPropertyValue", args = {})
-	public void testPreviousPropertyValue() {
-		// nothing to test
-	}
-
-	@Test
-	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "currentPropertyValue", args = {})
-	public void testCurrentPropertyValue() {
-		// nothing to test
-	}
-
-	
 	@Test
 	@UnitTestMethod(target = PersonPropertyUpdateEvent.class, name = "getPreviousPropertyValue", args = {})
 	public void testGetPreviousValue() {
