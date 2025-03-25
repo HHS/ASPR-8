bc66adb7669861e1c4076ea0bf858afdf4c582b8
shawnhatch <30730734+shawnhatch@users.noreply.github.com>
2025-03-25 17:55:42 -0400

    Integer sets (#301)
    * Added new implementor class for IntSetPeopleContainer
    
    * Improved new int set people container implementation.
    
    * Refactored IntSetPeopleContainer to reduce its memory use.

--- simulation/src/main/java/gov/hhs/aspr/ms/gcm/simulation/plugins/people/support/containers/IntSetPeopleContainer.java
+++ simulation/src/main/java/gov/hhs/aspr/ms/gcm/simulation/plugins/people/support/containers/IntSetPeopleContainer.java
@@ -1,17 +1,104 @@
 package gov.hhs.aspr.ms.gcm.simulation.plugins.people.support.containers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.math3.random.RandomGenerator;
 
+import gov.hhs.aspr.ms.gcm.simulation.plugins.people.datamanagers.PeopleDataManager;
+import gov.hhs.aspr.ms.gcm.simulation.plugins.people.support.PersonError;
 import gov.hhs.aspr.ms.gcm.simulation.plugins.people.support.PersonId;
+import gov.hhs.aspr.ms.util.errors.ContractException;
 
 /**
- * PeopleContainer implementor that uses hash-bucketed ArrayLists and an
- * array-based tree to contain the people. Uses ~ 45 bits per person contained.
+ * PeopleContainer implementor that uses hash-bucketed arrays and an array-based
+ * tree to contain the people. Uses ~ 45 bits per person contained under both 4
+ * byte and 8 byte pointer reference regimes.
  */
 public final class IntSetPeopleContainer implements PeopleContainer {
+
+	/*
+	 * A container for a set of int values that wraps an array, supports add, remove
+	 * and containment operations at a much lower memory cost compared to a
+	 * List<Integer>.
+	 */
+	private static class Bucket {
+		private int size;
+		private int[] values;
+
+		/*
+		 * Precondition: The value must not already be a member of this bucket.
+		 * 
+		 * Adds the given value WITHOUT checking for the existence of the value in the
+		 * contained array.
+		 */
+		public void unsafeAdd(int value) {
+			if (values == null) {
+				values = new int[1];
+			} else {
+				if (size == values.length) {
+					int newLength;
+					if (size == 1) {
+						newLength = 2;
+					} else {
+						newLength = (values.length * 3) / 2;
+					}
+					values = Arrays.copyOf(values, newLength);
+				}
+			}
+			values[size++] = value;
+		}
+
+		/*
+		 * Removes the value from the contained array if it can be found. Returns true
+		 * if and only if the value was found.
+		 */
+		public boolean remove(int value) {
+			int index = indexOf(value);
+			if (index >= 0) {
+				size--;
+				if (size > index) {
+					System.arraycopy(values, index + 1, values, index, size - index);
+				}
+				if (size * 2 < values.length) {
+					values = Arrays.copyOf(values, size);
+				}
+				return true;
+			}
+			return false;
+		}
+
+		/*
+		 * Returns true if and only if the value is contained.
+		 */
+		public boolean contains(int value) {
+			return indexOf(value) >= 0;
+		}
+
+		private int indexOf(int value) {
+			for (int i = 0; i < size; i++) {
+				if (values[i] == value) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		/*
+		 * Returns the value at the given index.
+		 */
+		public int get(int index) {
+			return values[index];
+		}
+
+		/*
+		 * Returns the number of values in this bucket.
+		 */
+		public int size() {
+			return size;
+		}
+	}
 
 	/**
 	 * The general best practice bucket depth for ArrayIntSets containing millions
@@ -22,113 +109,11 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 	/*
 	 * An array of ArrayLists that hold the values in the set.
 	 */
-	private List<PersonId>[] buckets;
+	private Bucket[] buckets;
 
 	private int[] tree;
 
-	/*
-	 * An array that is the same length as the values array that tracks the maximum
-	 * size each ArrayList instance in values has reached during its life-span. This
-	 * value is used to trigger an occasional rebuild of these ArrayLists to reduce
-	 * instance size.
-	 */
-	// private int[] maxSizes;
-	private static enum MaxMode {
-		BYTE, SHORT, INT
-	}
-
-	private static class MaxSizeManager {
-
-		private MaxMode maxMode = MaxMode.BYTE;
-
-		private int[] iMaxSizes;
-		private short[] sMaxSizes;
-		private byte[] bMaxSizes;
-
-		public MaxSizeManager(int capacity) {
-			bMaxSizes = new byte[capacity];
-		}
-
-		public void setMaxSize(int index, int size) {
-
-			switch (maxMode) {
-			case BYTE:
-				if (size > Byte.MAX_VALUE) {
-					if (size > Short.MAX_VALUE) {
-						int bLength = bMaxSizes.length;
-						iMaxSizes = new int[bLength];
-						for (int i = 0; i < bLength; i++) {
-							iMaxSizes[i] = bMaxSizes[i];
-						}
-						bMaxSizes = null;
-						maxMode = MaxMode.INT;
-
-					} else {
-						int bLength = bMaxSizes.length;
-						sMaxSizes = new short[bLength];
-						for (int i = 0; i < bLength; i++) {
-							sMaxSizes[i] = bMaxSizes[i];
-						}
-						bMaxSizes = null;
-						maxMode = MaxMode.SHORT;
-
-					}
-				}
-
-				break;
-			case INT:
-				// do nothing
-				break;
-			case SHORT:
-				if (size > Short.MAX_VALUE) {
-					int bLength = bMaxSizes.length;
-					iMaxSizes = new int[bLength];
-					for (int i = 0; i < bLength; i++) {
-						iMaxSizes[i] = bMaxSizes[i];
-					}
-					sMaxSizes = null;
-					maxMode = MaxMode.INT;
-				}
-				break;
-			default:
-
-				throw new RuntimeException("unhandled case " + maxMode);
-			}
-
-			switch (maxMode) {
-			case BYTE:
-				bMaxSizes[index] = (byte) size;
-				break;
-			case INT:
-				iMaxSizes[index] = size;
-				break;
-			case SHORT:
-				sMaxSizes[index] = (short) size;
-				break;
-			default:
-
-				throw new RuntimeException("unhandled case " + maxMode);
-			}
-		}
-
-		public int getMaxSize(int index) {
-			switch (maxMode) {
-			case BYTE:
-				return bMaxSizes[index];
-
-			case INT:
-				return iMaxSizes[index];
-
-			case SHORT:
-				return sMaxSizes[index];
-			default:
-
-				throw new RuntimeException("unhandled case " + maxMode);
-			}
-		}
-	}
-
-	private MaxSizeManager maxSizeManager;
+	private final PeopleDataManager peopleDataManager;
 
 	/*
 	 * The number of Integers stored in this set
@@ -145,9 +130,14 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 	 * Constructs an ArrayIntSet having the DEFAULT_TARGET_DEPTH and tolerance of
 	 * duplicate values.
 	 * 
-	 * @throws IllegalArgumentException the target depth is not positive
+	 * @throws ContractException {@linkplain PersonError#NULL_PEOPLE_DATA_MANAGER}
+	 *                           if the person data view is null
 	 */
-	public IntSetPeopleContainer() {
+	public IntSetPeopleContainer(PeopleDataManager peopleDataManager) {
+		if (peopleDataManager == null) {
+			throw new ContractException(PersonError.NULL_PEOPLE_DATA_MANAGER);
+		}
+		this.peopleDataManager = peopleDataManager;
 		this.targetDepth = DEFAULT_TARGET_DEPTH;
 	}
 
@@ -155,14 +145,10 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 	 * Grows the values and maxSizes arrays to achieve an average bucket depth that
 	 * closer to the target bucket depth.
 	 */
-	@SuppressWarnings("unchecked")
 	private void grow() {
 		if (buckets == null) {
 			// establish a single bucket
-			buckets = new List[1];
-			// maxSizes = new int[1];
-			maxSizeManager = new MaxSizeManager(1);
-
+			buckets = new Bucket[1];
 			tree = new int[2];
 		} else {
 			// double the number of buckets
@@ -185,40 +171,36 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 	 * Rebuilds the buckets to the new size for the values and maxSizes arrays.
 	 */
 	private void rebuild(int newSize) {
-		@SuppressWarnings("unchecked")
 		/*
 		 * create a new values array to the new size
 		 */
-		List<PersonId>[] newValues = new List[newSize];
-		/*
-		 * Rebuild the maxSizes array to the correct length. The old values in the
-		 * maxSizes array can be forgotten.
-		 */
-		// maxSizes = new int[newValues.length];
-		maxSizeManager = new MaxSizeManager(newValues.length);
+		Bucket[] newBuckets = new Bucket[newSize];
+
 		/*
 		 * Place the values from the old values array into the new values array.
 		 */
 		tree = new int[newSize * 2];
 
 		for (int i = 0; i < buckets.length; i++) {
-			List<PersonId> list = buckets[i];
-			if (list != null) {
-				for (PersonId value : list) {
+			Bucket bucket = buckets[i];
+			if (bucket != null) {
+				int bucketSize = bucket.size();
+				for (int j = 0; j < bucketSize; j++) {
 					/*
 					 * Since the length of the values array is always a power of 2, we can use a
 					 * bit-wise math trick to calculate the modulus of value with values.length to
 					 * derive the index of the bucket where the value should be stored.
 					 */
-					int index = value.getValue() & (newValues.length - 1);
+					int value = bucket.get(j);
+					int index = value & (newBuckets.length - 1);
 					/*
 					 * Get the list where the value should be stored or create it if it does not yet
 					 * exist.
 					 */
-					List<PersonId> newList = newValues[index];
-					if (newList == null) {
-						newList = new ArrayList<>();
-						newValues[index] = newList;
+					Bucket newBucket = newBuckets[index];
+					if (newBucket == null) {
+						newBucket = new Bucket();
+						newBuckets[index] = newBucket;
 					}
 
 					int treeIndex = index + newSize;
@@ -228,9 +210,9 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 					}
 
 					/*
-					 * Place the value in the list.
+					 * Place the value in the bucket.
 					 */
-					newList.add(value);
+					newBucket.unsafeAdd(value);
 				}
 				/*
 				 * Null out the ArrayList instance to encourage the GC to collect the list now.
@@ -243,17 +225,7 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		 * We no longer need the old values array, so we replace it with the new values
 		 * array.
 		 */
-		buckets = newValues;
-		/*
-		 * Finally, we establish the maxSizes array values.
-		 */
-		for (int i = 0; i < buckets.length; i++) {
-			List<PersonId> list = buckets[i];
-			if (list != null) {
-				// maxSizes[i] = list.size();
-				maxSizeManager.setMaxSize(i, list.size());
-			}
-		}
+		buckets = newBuckets;
 	}
 
 	@Override
@@ -278,18 +250,19 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		 * The bucket index is value % values.length, but this is a bit faster since we
 		 * know the values array length is a power of two.
 		 */
-		int index = personId.getValue() & (buckets.length - 1);
+		int value = personId.getValue();
+		int index = value & (buckets.length - 1);
 
 		/*
 		 * Add the value to list located at the index
 		 */
-		List<PersonId> list = buckets[index];
-		if (list == null) {
-			list = new ArrayList<>();
-			buckets[index] = list;
+		Bucket bucket = buckets[index];
+		if (bucket == null) {
+			bucket = new Bucket();
+			buckets[index] = bucket;
 		}
 
-		list.add(personId);
+		bucket.unsafeAdd(value);
 
 		size++;
 
@@ -298,17 +271,6 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		while (treeIndex > 0) {
 			tree[treeIndex]++;
 			treeIndex /= 2;
-		}
-
-		/*
-		 * Update the maxSizes
-		 */
-		// if (maxSizes[index] < list.size()) {
-		// maxSizes[index] = list.size();
-		// }
-		if (maxSizeManager.getMaxSize(index) < list.size()) {
-			// maxSizes[index] = list.size();
-			maxSizeManager.setMaxSize(index, list.size());
 		}
 
 		/*
@@ -331,17 +293,18 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		 * The bucket index is value % values.length, but this is a bit faster since we
 		 * know the values array length is a power of two.
 		 */
-		int index = personId.getValue() & (buckets.length - 1);
+		int value = personId.getValue();
+		int index = value & (buckets.length - 1);
 		/*
 		 * Remove the value from the list
 		 */
-		List<PersonId> list = buckets[index];
+		Bucket bucket = buckets[index];
 
-		if (list == null) {
+		if (bucket == null) {
 			return false;
 		}
 
-		if (list.remove(personId)) {
+		if (bucket.remove(value)) {
 			size--;
 			/*
 			 * If the list is now less than half its maxSize in the past, then we should
@@ -354,15 +317,6 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 				treeIndex /= 2;
 			}
 
-			if (list.size() * 2 < maxSizeManager.getMaxSize(index)) {
-				if (list.size() > 0) {
-					buckets[index] = new ArrayList<>(list);
-					maxSizeManager.setMaxSize(index, list.size());
-				} else {
-					buckets[index] = null;
-					maxSizeManager.setMaxSize(index, 0);
-				}
-			}
 			/*
 			 * If the averageDepth is less than half the target depth then we should shrink.
 			 * (size/values.length)*2<targetDepth
@@ -381,9 +335,13 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 	public List<PersonId> getPeople() {
 		List<PersonId> result = new ArrayList<>(size);
 		if (buckets != null) {
-			for (List<PersonId> list : buckets) {
-				if (list != null) {
-					result.addAll(list);
+			for (Bucket bucket : buckets) {
+				if (bucket != null) {
+					int n = bucket.size();
+					for (int i = 0; i < n; i++) {
+						PersonId personId = peopleDataManager.getBoxedPersonId(bucket.get(i)).get();
+						result.add(personId);
+					}
 				}
 			}
 		}
@@ -427,8 +385,9 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		 * We have arrived at the element of the tree array that corresponds to desired
 		 * bucket
 		 */
-		List<PersonId> targetList = buckets[treeIndex - midTreeIndex];
-		return targetList.get(targetCount);
+		Bucket bucket = buckets[treeIndex - midTreeIndex];
+		int value = bucket.get(targetCount);
+		return peopleDataManager.getBoxedPersonId(value).get();
 
 	}
 
@@ -442,12 +401,13 @@ public final class IntSetPeopleContainer implements PeopleContainer {
 		if (buckets == null) {
 			return false;
 		}
-		int index = personId.getValue() & (buckets.length - 1);
-		List<PersonId> list = buckets[index];
-		if (list == null) {
+		int value = personId.getValue();
+		int index = value & (buckets.length - 1);
+		Bucket bucket = buckets[index];
+		if (bucket == null) {
 			return false;
 		}
-		return list.contains(personId);
+		return bucket.contains(value);
 	}
 
 }
