5a2bc671031d94df6382107cbc74376ccf844887
travisrieglerleidos <travis.v.riegler@leidos.com>
2025-04-16 17:23:08 -0400

    Update equals and hashCode in BatchId and corresponding tests

--- simulation/src/test/java/gov/hhs/aspr/ms/gcm/simulation/plugins/materials/support/AT_BatchId.java
+++ simulation/src/test/java/gov/hhs/aspr/ms/gcm/simulation/plugins/materials/support/AT_BatchId.java
@@ -1,13 +1,18 @@
 package gov.hhs.aspr.ms.gcm.simulation.plugins.materials.support;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.commons.math3.random.RandomGenerator;
 import org.junit.jupiter.api.Test;
 
 import gov.hhs.aspr.ms.util.annotations.UnitTestConstructor;
 import gov.hhs.aspr.ms.util.annotations.UnitTestMethod;
+import gov.hhs.aspr.ms.util.random.RandomGeneratorProvider;
 
 public class AT_BatchId {
 
@@ -43,17 +48,45 @@ public class AT_BatchId {
 	@Test
 	@UnitTestMethod(target = BatchId.class,name = "equals", args = { Object.class })
 	public void testEquals() {
-		for (int i = 0; i < 10; i++) {
-			BatchId batchA = new BatchId(i);
+		RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(8980821418377306870L);
+
+		// never equal to another type
+		for (int i = 0; i < 30; i++) {
+			BatchId batchId = getRandomBatchId(randomGenerator.nextLong());
+			assertFalse(batchId.equals(new Object()));
+		}
+
+		// never equal to null
+		for (int i = 0; i < 30; i++) {
+			BatchId batchId = getRandomBatchId(randomGenerator.nextLong());
+			assertFalse(batchId.equals(null));
+		}
+
+		// reflexive
+		for (int i = 0; i < 30; i++) {
+			BatchId batchId = getRandomBatchId(randomGenerator.nextLong());
+			assertTrue(batchId.equals(batchId));
+		}
+
+		// symmetric, transitive, consistent
+		for (int i = 0; i < 30; i++) {
+			long seed = randomGenerator.nextLong();
+			BatchId batchId1 = getRandomBatchId(seed);
+			BatchId batchId2 = getRandomBatchId(seed);
+			assertFalse(batchId1 == batchId2);
 			for (int j = 0; j < 10; j++) {
-				BatchId batchB = new BatchId(j);				
-				if (i == j) {
-					assertEquals(batchA,batchB);
-				} else {
-					assertNotEquals(batchA,batchB);
-				}
+				assertTrue(batchId1.equals(batchId2));
+				assertTrue(batchId2.equals(batchId1));
 			}
 		}
+
+		// different inputs yield unequal batchIds
+		Set<BatchId> set = new LinkedHashSet<>();
+		for (int i = 0; i < 100; i++) {
+			BatchId batchId = getRandomBatchId(randomGenerator.nextLong());
+			set.add(batchId);
+		}
+		assertEquals(100, set.size());
 	}
 
 	@Test
@@ -68,10 +101,26 @@ public class AT_BatchId {
 	@Test
 	@UnitTestMethod(target = BatchId.class,name = "hashCode", args = {})
 	public void testHashCode() {
-		for (int i = 0; i < 10; i++) {
-			BatchId batch = new BatchId(i);
-			assertEquals(i, batch.hashCode());
+		RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(2657771508465183354L);
+
+		// equal objects have equal hash codes
+		for (int i = 0; i < 30; i++) {
+			long seed = randomGenerator.nextLong();
+			BatchId batchId1 = getRandomBatchId(seed);
+			BatchId batchId2 = getRandomBatchId(seed);
+
+			assertEquals(batchId1, batchId2);
+			assertEquals(batchId1.hashCode(), batchId2.hashCode());
 		}
+
+		// hash codes are reasonably distributed
+		Set<Integer> hashCodes = new LinkedHashSet<>();
+		for (int i = 0; i < 100; i++) {
+			BatchId batchId = getRandomBatchId(randomGenerator.nextLong());
+			hashCodes.add(batchId.hashCode());
+		}
+
+		assertEquals(100, hashCodes.size());
 	}
 
 	@Test
@@ -82,4 +131,9 @@ public class AT_BatchId {
 			assertEquals(Integer.toString(i), batch.toString());
 		}
 	}
+
+	private BatchId getRandomBatchId(long seed) {
+        RandomGenerator randomGenerator = RandomGeneratorProvider.getRandomGenerator(seed);
+        return new BatchId(randomGenerator.nextInt());
+    }
 }
